e5d5aa79325ffda1b16be4c04068cdf37779adff
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-10-27 11:56:11 +0300

    use authentication in app api

--- IdentityServerSample.AppApi/Program.cs
+++ IdentityServerSample.AppApi/Program.cs
@@ -3,8 +3,13 @@
 // See LICENSE in the project root for license information.
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddAuthentication("Bearer")
+                .AddJwtBearer(options => options.Authority = "https://localhost:7214");
+
 var app = builder.Build();
 
+app.UseAuthentication();
 app.MapControllers();
-
 app.Run();
