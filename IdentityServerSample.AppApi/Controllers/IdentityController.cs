36ea606939f200029a43b6e80257ea4e8fc4c831
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-10-27 11:48:34 +0300

    add id contr to app api

---
+++ IdentityServerSample.AppApi/Controllers/IdentityController.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.AppApi.Controllers
+{
+  using Microsoft.AspNetCore.Mvc;
+
+  [Route("api/identity")]
+  public sealed class IdentityController : ControllerBase
+  {
+    [HttpGet]
+    public IActionResult Get()
+    {
+      return Ok(new
+      {
+        name = User.Identity?.Name,
+        claims = User.Claims.Select(claim => claim.Value).ToArray(),
+      });
+    }
+  }
+}
