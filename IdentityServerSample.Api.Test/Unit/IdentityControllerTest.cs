53294e003eec4976079634a0dd3d3390cb73648f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-10-28 11:08:46 +0300

    add fist test

---
+++ IdentityServerSample.Api.Test/Unit/IdentityControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using IdentityServerSample.Api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServerSample.Api.Test.Unit
+{
+  [TestClass]
+  public sealed class IdentityControllerTest
+  {
+#pragma warning disable CS8618
+    private IdentityController _controller;
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      _controller = new IdentityController();
+    }
+
+    [TestMethod]
+    public void GetTest()
+    {
+      var actionResult = _controller.Get();
+
+      Assert.IsNotNull(actionResult);
+    }
+  }
+}
