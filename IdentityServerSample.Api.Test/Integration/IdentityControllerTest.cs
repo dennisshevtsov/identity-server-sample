98410a82232e99faa16ec0536f84a24bcab09b69
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-10-31 12:15:39 +0300

    clean id test

--- IdentityServerSample.Api.Test/Integration/IdentityControllerTest.cs
+++ IdentityServerSample.Api.Test/Integration/IdentityControllerTest.cs
@@ -1,34 +1,31 @@
-﻿using IdentityModel.Client;
-using IdentityServerSample.Api.Controllers;
-using Microsoft.AspNetCore.Http;
-using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
 
 namespace IdentityServerSample.Api.Test.Integration
 {
+  using IdentityModel.Client;
+
   [TestClass]
   public sealed class IdentityControllerTest
   {
 #pragma warning disable CS8618
-    private HttpClient _httpClient;
+    private HttpClient _identityHttpClient;
 #pragma warning restore CS8618
 
     [TestInitialize]
     public void Initialize()
     {
-      _httpClient = new HttpClient();
+      _identityHttpClient = new HttpClient
+      {
+        BaseAddress = new Uri("http://localhost:5214"),
+      };
     }
 
     [TestMethod]
     public async Task Test()
     {
-      var discovery = await _httpClient.GetDiscoveryDocumentAsync("http://localhost:5214");
+      var discovery = await _identityHttpClient.GetDiscoveryDocumentAsync();
 
       Assert.IsFalse(discovery.IsError);
     }
