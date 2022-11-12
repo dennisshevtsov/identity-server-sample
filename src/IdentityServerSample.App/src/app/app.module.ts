99660861fa040a16d289b77e64e3b9fa897dad60
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-11-12 12:05:20 +0300

    clean ang app

--- src/IdentityServerSample.App/src/app/app.module.ts
+++ src/IdentityServerSample.App/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule,        } from '@angular/core';
+import { BrowserModule,   } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent, } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+  ],
 })
 export class AppModule { }
