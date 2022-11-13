a9fd077895dd7526c07b868e2acb2256c429ab79
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-11-13 13:42:40 +0300

    add oidc client

--- src/IdentityServerSample.App/src/app/app.component.ts
+++ src/IdentityServerSample.App/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component,              } from '@angular/core';
 import { FormBuilder, FormGroup, } from '@angular/forms';
+import { UserManager,            } from 'oidc-client-ts';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +8,32 @@ import { FormBuilder, FormGroup, } from '@angular/forms';
   styleUrls: [
     './app.component.css',
   ],
+  providers: [
+    {
+      provide: UserManager,
+      useFactory: () => new UserManager({
+        authority: '',
+        client_id: '',
+        redirect_uri: '',
+        response_type: 'code',
+        scope: '',
+      }),
+    }
+  ]
 })
 export class AppComponent {
   private formValue: undefined | FormGroup;
 
-  public constructor(private readonly fb: FormBuilder) { }
+  public constructor(
+    private readonly fb: FormBuilder,
+    private readonly um: UserManager) { }
 
   public get form(): FormGroup {
     return this.formValue ?? (this.formValue = this.buildForm());
   }
 
   public onSignIn(): void {
-    console.log(JSON.stringify(this.form.value));
+    this.um.signinRedirect();
   }
 
   private buildForm(): FormGroup {
