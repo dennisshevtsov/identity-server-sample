a60e3ff7fe9d5279b18f1cccec6df9e26b2b19e9
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-11-15 18:47:15 +0300

    add account contr

---
+++ src/IdentityServerSample.IdentityApi/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.WebApi.Controllers
+{
+  using Microsoft.AspNetCore.Authorization;
+  using Microsoft.AspNetCore.Mvc;
+
+  [AllowAnonymous]
+  public sealed class AccountController : ControllerBase
+  {
+    [HttpGet]
+    public IActionResult Login(string returnUrl)
+    {
+      return Redirect(returnUrl);
+    }
+  }
+}
