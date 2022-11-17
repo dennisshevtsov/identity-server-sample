6bf3ece28be0d011a1d01812cfc770469e6391f6
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-11-17 11:43:28 +0300

    add web app

---
+++ IdentityServerSample.WebApp/Program.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapFallbackToFile("index.html");
+app.Run();
