af4c78c37a46542799c5bf3d83332622b02085b1
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-11-21 13:30:02 +0300

    add sing in page

---
+++ src/IdentityServerSample.IdentityApp/Pages/SignInPage.cshtml.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.IdentityApp.Pages
+{
+  using IdentityServer4;
+  using Microsoft.AspNetCore.Mvc;
+  using Microsoft.AspNetCore.Mvc.RazorPages;
+
+  public class SignInPageModel : PageModel
+  {
+    [BindProperty(SupportsGet = true)]
+    public string? ReturnUrl { get; set; }
+
+    [BindProperty(SupportsGet = false)]
+    public string? Email { get; set; }
+
+    [BindProperty(SupportsGet = false)]
+    public string? Password { get; set; }
+
+    [BindProperty(SupportsGet = false)]
+    public bool RememberMe { get; set; }
+
+    public async Task<IActionResult> OnGet()
+    {
+      await HttpContext.SignInAsync(new IdentityServerUser("test")
+      {
+        DisplayName = "test",
+      });
+
+      return Redirect(ReturnUrl!);
+    }
+  }
+}
