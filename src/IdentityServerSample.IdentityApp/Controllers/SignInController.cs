27207fad4bd94b6ad667188004b33e2cecc6123e
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-01 00:15:17 +0300

    split acc contr

---
+++ src/IdentityServerSample.IdentityApp/Controllers/SignInController.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.IdentityApp.Controllers
+{
+  using IdentityServer4;
+  using IdentityServer4.Test;
+  using Microsoft.AspNetCore.Mvc;
+
+  using IdentityServerSample.IdentityApp.Defaults;
+  using IdentityServerSample.IdentityApp.ViewModels;
+
+  /// <summary>Provides a simple API to handle HTTP requests.</summary>
+  [Route(Routing.AccountRoute)]
+  public sealed class SignInController : Controller
+  {
+    private readonly TestUserStore _userStore;
+
+    /// <summary>Inititalizes a new instance of the <see cref="IdentityServerSample.IdentityApp.Controllers.SignInController"/> class.</summary>
+    /// <param name="userStore">An object that represents a store for test users.</param>
+    public SignInController(TestUserStore userStore)
+    {
+      _userStore = userStore ?? throw new ArgumentNullException(nameof(userStore));
+    }
+
+    /// <summary>Handles the get sign-in page request.</summary>
+    /// <param name="vm">An object that represents details of sign-in credentials.</param>
+    /// <returns>An object that defines a contract that represents the result of an action method.</returns>
+    [HttpGet(Routing.SignInRoute)]
+    public IActionResult Get(SignInViewModel vm)
+    {
+      ModelState.Clear();
+
+      return View("SignInView", vm);
+    }
+
+    /// <summary>Handles the post sign-in credentials request.</summary>
+    /// <param name="vm">An object that represents details of sign-in credentials.</param>
+    /// <returns>An object that represents an asynchronous operation.</returns>
+    [HttpPost(Routing.SignInRoute)]
+    public async Task<IActionResult> Post(SignInViewModel vm)
+    {
+      if (ModelState.IsValid)
+      {
+        if (_userStore.ValidateCredentials(vm.Email, vm.Password))
+        {
+          var testUser = _userStore.FindByUsername(vm.Email);
+          var identityServerUser = new IdentityServerUser(testUser.SubjectId)
+          {
+            DisplayName = testUser.Username,
+          };
+
+          await HttpContext.SignInAsync(identityServerUser);
+
+          return Redirect(vm.ReturnUrl!);
+        }
+
+        ModelState.AddModelError(
+          nameof(SignInViewModel.Email), "The credentials is not valid.");
+      }
+
+      return View("SignInView", vm);
+    }
+  }
+}
--- src/IdentityServerSample.IdentityApp/Controllers/SignOutController.cs
+++ src/IdentityServerSample.IdentityApp/Controllers/SignOutController.cs
@@ -4,12 +4,8 @@
 
 namespace IdentityServerSample.IdentityApp.Controllers
 {
-  using System;
-
-  using IdentityServer4;
   using IdentityServer4.Models;
   using IdentityServer4.Services;
-  using IdentityServer4.Test;
   using Microsoft.AspNetCore.Authentication;
   using Microsoft.AspNetCore.Mvc;
 
@@ -18,67 +14,25 @@ namespace IdentityServerSample.IdentityApp.Controllers
 
   /// <summary>Provides a simple API to handle HTTP requests.</summary>
   [Route(Routing.AccountRoute)]
-  public class AccountController : Controller
+  public sealed class SignOutController : Controller
   {
-    private readonly TestUserStore _userStore;
     private readonly IIdentityServerInteractionService _identityServerInteractionService;
 
-    /// <summary>Inititalizes a new instance of the <see cref="IdentityServerSample.IdentityApp.Controllers.AccountController"/> class.</summary>
+    /// <summary>Inititalizes a new instance of the <see cref="IdentityServerSample.IdentityApp.Controllers.SignOutController"/> class.</summary>
     /// <param name="userStore">An object that represents a store for test users.</param>
     /// <param name="identityServerInteractionService">An object that provides a simple API to communicate with IdentityServer.</param>
-    public AccountController(
-      TestUserStore userStore,
+    public SignOutController(
       IIdentityServerInteractionService identityServerInteractionService)
     {
-      _userStore = userStore ?? throw new ArgumentNullException(nameof(userStore));
       _identityServerInteractionService = identityServerInteractionService ??
         throw new ArgumentNullException(nameof(identityServerInteractionService));
-    }
-
-    /// <summary>Handles the get sign-in page request.</summary>
-    /// <param name="vm">An object that represents details of sign-in credentials.</param>
-    /// <returns>An object that defines a contract that represents the result of an action method.</returns>
-    [HttpGet(Routing.SignInRoute)]
-    public IActionResult GetSignIn(SignInViewModel vm)
-    {
-      ModelState.Clear();
-
-      return View("SignInView", vm);
-    }
-
-    /// <summary>Handles the post sign-in credentials request.</summary>
-    /// <param name="vm">An object that represents details of sign-in credentials.</param>
-    /// <returns>An object that represents an asynchronous operation.</returns>
-    [HttpPost(Routing.SignInRoute)]
-    public async Task<IActionResult> PostSignIn(SignInViewModel vm)
-    {
-      if (ModelState.IsValid)
-      {
-        if (_userStore.ValidateCredentials(vm.Email, vm.Password))
-        {
-          var testUser = _userStore.FindByUsername(vm.Email);
-          var identityServerUser = new IdentityServerUser(testUser.SubjectId)
-          {
-            DisplayName = testUser.Username,
-          };
-
-          await HttpContext.SignInAsync(identityServerUser);
-
-          return Redirect(vm.ReturnUrl!);
-        }
-
-        ModelState.AddModelError(
-          nameof(SignInViewModel.Email), "The credentials is not valid.");
-      }
-
-      return View("SignInView", vm);
     }
 
     /// <summary>Handles the get sign-out page request.</summary>
     /// <param name="vm">An object that represents details of a sing-out request.</param>
     /// <returns>An object that defines a contract that represents the result of an action method.</returns>
     [HttpGet(Routing.SignOutRoute)]
-    public async Task<IActionResult> GetSignOut(SignOutViewModel vm)
+    public async Task<IActionResult> Get(SignOutViewModel vm)
     {
       var logoutRequest =
         await _identityServerInteractionService.GetLogoutContextAsync(vm.SignOutId)!;
@@ -95,7 +49,7 @@ namespace IdentityServerSample.IdentityApp.Controllers
     /// <param name="vm">An object that represents details of a sing-out request.</param>
     /// <returns>An object that represents an asynchronous operation.</returns>
     [HttpPost(Routing.SignOutRoute)]
-    public async Task<IActionResult> PostSignOut(SignOutViewModel vm)
+    public async Task<IActionResult> Post(SignOutViewModel vm)
     {
       var logoutRequest =
         await _identityServerInteractionService.GetLogoutContextAsync(vm.SignOutId)!;
