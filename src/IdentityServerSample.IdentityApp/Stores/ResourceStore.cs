6625fb27c47c7c8f8b7a5e71082e3ea99f02ad76
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-07 13:22:47 +0300

    imple resource srote

--- src/IdentityServerSample.IdentityApp/Stores/ResourceStore.cs
+++ src/IdentityServerSample.IdentityApp/Stores/ResourceStore.cs
@@ -4,54 +4,97 @@
 
 namespace IdentityServerSample.IdentityApp.Stores
 {
+  using System.Linq;
+
   using IdentityServer4.Models;
   using IdentityServer4.Stores;
 
   /// <summary>Provides a simple API to query resources.</summary>
   public sealed class ResourceStore : IResourceStore
   {
-    /// <summary>Gets identity resources by scope name.</summary>
-    /// <param name="scopeNames"></param>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
-    public Task<IEnumerable<IdentityResource>> FindIdentityResourcesByScopeNameAsync(IEnumerable<string> scopeNames)
+    private readonly IEnumerable<IdentityResource> _identityResources;
+    private readonly IEnumerable<ApiResource> _apiResources;
+    private readonly IEnumerable<ApiScope> _apiScopes;
+
+    /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.IdentityApp.Stores.ResourceStore"/> class.</summary>
+    /// <param name="configuration">An object that represents a set of key/value application configuration properties.</param>
+    public ResourceStore(IConfiguration configuration)
     {
-      throw new NotImplementedException();
+      _identityResources = new IdentityResource[]
+      {
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile(),
+      };
+
+      _apiResources = new[]
+      {
+        new ApiResource
+        {
+          Name = configuration["ApiResource_Name"],
+          DisplayName = configuration["ApiResource_DisplayName"],
+          Scopes =
+          {
+            configuration["ApiScope_Name"],
+          },
+        },
+      };
+
+      _apiScopes = new[]
+      {
+        new ApiScope(configuration["ApiScope_Name"], configuration["ApiScope_DisplayName"]),
+      };
+    }
+
+    /// <summary>Gets identity resources by scope name.</summary>
+    /// <param name="scopeNames">An object that represents a collection of scope names.</param>
+    /// <returns>An object that represents an asynchronous operation.</returns>
+    public Task<IEnumerable<IdentityResource>> FindIdentityResourcesByScopeNameAsync(
+      IEnumerable<string> scopeNames)
+    {
+      return Task.FromResult(_identityResources.Where(resource => scopeNames.Contains(resource.Name)));
     }
 
     /// <summary>Gets API scopes by scope name.</summary>
-    /// <param name="scopeNames"></param>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
-    public Task<IEnumerable<ApiScope>> FindApiScopesByNameAsync(IEnumerable<string> scopeNames)
+    /// <param name="scopeNames">An object that represents a collection of scope names.</param>
+    /// <returns>An object that represents an asynchronous operation.</returns>
+    public Task<IEnumerable<ApiScope>> FindApiScopesByNameAsync(
+      IEnumerable<string> scopeNames)
     {
-      throw new NotImplementedException();
+      return Task.FromResult(_apiScopes.Where(scope => scopeNames.Contains(scope.Name)));
     }
 
     /// <summary>Gets API resources by scope name.</summary>
-    /// <param name="scopeNames"></param>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
-    public Task<IEnumerable<ApiResource>> FindApiResourcesByScopeNameAsync(IEnumerable<string> scopeNames)
+    /// <param name="scopeNames">An object that represents a collection of scope names.</param>
+    /// <returns>An object that represents an asynchronous operation.</returns>
+    public Task<IEnumerable<ApiResource>> FindApiResourcesByScopeNameAsync(
+      IEnumerable<string> scopeNames)
     {
-      throw new NotImplementedException();
+      var apiRecources = _apiResources;
+
+      if (scopeNames != null && scopeNames.Any())
+      {
+        apiRecources = apiRecources.Where(
+          resource => resource.Scopes != null &&
+                      resource.Scopes.Any(scope => scopeNames.Contains(scope)));
+      }
+
+      return Task.FromResult(apiRecources);
     }
 
     /// <summary>Gets API resources by API resource name.</summary>
-    /// <param name="apiResourceNames"></param>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
-    public Task<IEnumerable<ApiResource>> FindApiResourcesByNameAsync(IEnumerable<string> apiResourceNames)
+    /// <param name="apiResourceNames">An object that represents a collection of API resource names.</param>
+    /// <returns>An object that represents an asynchronous operation.</returns>
+    public Task<IEnumerable<ApiResource>> FindApiResourcesByNameAsync(
+      IEnumerable<string> apiResourceNames)
     {
-      throw new NotImplementedException();
+      return Task.FromResult(_apiResources.Where(resource => apiResourceNames.Contains(resource.Name)));
     }
 
     /// <summary>Gets all resources.</summary>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
+    /// <returns>An object that represents an asynchronous operation.</returns>
     public Task<Resources> GetAllResourcesAsync()
     {
-      throw new NotImplementedException();
+      return Task.FromResult(new Resources(_identityResources, _apiResources, _apiScopes));
     }
   }
 }
