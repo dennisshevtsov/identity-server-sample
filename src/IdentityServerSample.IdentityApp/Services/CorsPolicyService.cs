fba6b56bb7a6d68488d3f22aee506131507e041a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-07 13:43:38 +0300

    move origin to settings

--- src/IdentityServerSample.IdentityApp/Services/CorsPolicyService.cs
+++ src/IdentityServerSample.IdentityApp/Services/CorsPolicyService.cs
@@ -4,17 +4,29 @@
 
 namespace IdentityServerSample.IdentityApp.Services
 {
+  using System;
+
   using IdentityServer4.Services;
+  using Microsoft.Extensions.Configuration;
 
   /// <summary>Provides a simple API to determine if CORS is allowed.</summary>
   public sealed class CorsPolicyService : ICorsPolicyService
   {
+    private IConfiguration _configuration;
+
+    /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.IdentityApp.Services.CorsPolicyService"/> class.</summary>
+    /// <param name="configuration">An object that represents a set of key/value application configuration properties.</param>
+    public CorsPolicyService(IConfiguration configuration)
+    {
+      _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+
     /// <summary>Checks if an origin is allowed.</summary>
     /// <param name="origin">An object that represents an origin.</param>
     /// <returns>An object that represents an asynchronous operation.</returns>
     public Task<bool> IsOriginAllowedAsync(string origin)
     {
-      return Task.FromResult(origin == "http://localhost:4200");
+      return Task.FromResult(origin == _configuration["WebApp_Url"]);
     }
   }
 }
