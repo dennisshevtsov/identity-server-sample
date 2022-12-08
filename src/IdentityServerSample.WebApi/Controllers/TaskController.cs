daebe046e9df277e894cd06e3405d3d2101c85b3
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-08 12:40:58 +0300

    add task contr

--- src/IdentityServerSample.WebApi/Controllers/TaskController.cs
+++ src/IdentityServerSample.WebApi/Controllers/TaskController.cs
@@ -19,7 +19,28 @@ namespace IdentityServerSample.WebApi.Controllers
     [ProducesResponseType(typeof(GetTasksResponseDto), StatusCodes.Status200OK)]
     public IActionResult GetTasks()
     {
-      return Ok();
+      return Ok(new GetTasksResponseDto
+      {
+        Total = 1,
+        PageNo = 1,
+        PageCount = 1,
+        Tasks = new[]
+        {
+          new GetTasksResponseDto.TaskDto
+          {
+            TaskId = 1,
+            Title = "Test",
+            Assigned = new GetTasksResponseDto.UserDto
+            {
+              UserId = 1,
+              Name = "John Smith",
+            },
+            Deadline = DateTime.Now.AddDays(10),
+            Priority = Priority.Medium,
+            Status = Status.ToDo,
+          },
+        },
+      });
     }
   }
 }
