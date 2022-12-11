028e4dd329c54d3a5c6ff272af16d56f5abd1416
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-11 23:06:27 +0300

    add scope config

---
+++ src/IdentityServerSample.Infrastructure/Configurations/ScopeEntityTypeConfiguration.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.Infrastructure.Configurations
+{
+  using Microsoft.EntityFrameworkCore;
+  using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+  using IdentityServerSample.ApplicationCore.Entities;
+
+  /// <summary>Allows configuration for an entity type.</summary>
+  public sealed class ScopeEntityTypeConfiguration : IEntityTypeConfiguration<ScopeEntity>
+  {
+    private readonly string _containerName;
+
+    /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.Infrastructure.Configurations.ClientEntityTypeConfiguration"/> class.</summary>
+    /// <param name="containerName">An object that represents a name of a container.</param>
+    public ScopeEntityTypeConfiguration(string containerName)
+    {
+      _containerName = containerName ?? throw new ArgumentNullException(nameof(containerName));
+    }
+
+    /// <summary>Configures the entity of the <see cref="IdentityServerSample.ApplicationCore.Entities.ScopeEntity"/> type.</summary>
+    /// <param name="builder">An object that provides a simple API to configure the entity type.</param>
+    public void Configure(EntityTypeBuilder<ScopeEntity> builder)
+    {
+      builder.ToContainer(_containerName);
+
+      builder.HasKey(entity => entity.Name);
+      builder.HasPartitionKey(entity => entity.Name);
+
+      builder.HasNoDiscriminator();
+
+      builder.Property(entity => entity.Name).ToJsonProperty("name");
+      builder.Property(entity => entity.DisplayName).ToJsonProperty("displayName");
+      builder.Property(entity => entity.Description).ToJsonProperty("description");
+    }
+  }
+}
