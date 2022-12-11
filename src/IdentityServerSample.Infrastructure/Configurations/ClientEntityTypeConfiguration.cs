5c9da72042888c53eb298b5d900a8e75189e75b6
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-11 12:26:29 +0300

    add client config

---
+++ src/IdentityServerSample.Infrastructure/Configurations/ClientEntityTypeConfiguration.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.Infrastructure.Configurations
+{
+  using Microsoft.EntityFrameworkCore;
+  using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+  using IdentityServerSample.ApplicationCore.Entities;
+
+  /// <summary>Allows configuration for an entity type.</summary>
+  public sealed class ClientEntityTypeConfiguration : IEntityTypeConfiguration<ClientEntity>
+  {
+    private readonly string _containerName;
+
+    /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.Infrastructure.Configurations.ClientEntityTypeConfiguration"/> class.</summary>
+    /// <param name="containerName">An object that represents a name of a container.</param>
+    public ClientEntityTypeConfiguration(string containerName)
+    {
+      _containerName = containerName ?? throw new ArgumentNullException(nameof(containerName));
+    }
+
+    /// <summary>Configures the entity of the <see cref="IdentityServerSample.ApplicationCore.Entities.ClientEntity"/> type.</summary>
+    /// <param name="builder">An object that provides a simple API to configure the entity type.</param>
+    public void Configure(EntityTypeBuilder<ClientEntity> builder)
+    {
+      builder.ToContainer(_containerName);
+
+      builder.HasKey(entity => entity.ClientId);
+      builder.HasPartitionKey(entity => entity.ClientId);
+
+      builder.HasNoDiscriminator();
+
+      builder.Property(entity => entity.ClientId).ToJsonProperty("id");
+      builder.Property(entity => entity.Name).ToJsonProperty("name");
+
+      builder.OwnsMany(entity => entity.Scopes).ToJsonProperty("scopes");
+      builder.OwnsMany(entity => entity.RedirectUris).ToJsonProperty("redirectUris");
+      builder.OwnsMany(entity => entity.PostRedirectUris).ToJsonProperty("postRedirectUris");
+    }
+  }
+}
