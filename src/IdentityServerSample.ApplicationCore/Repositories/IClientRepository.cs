43d574e2a5e550a487eb1092638ce705a3d09f68
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-11 12:32:01 +0300

    impl client repo

--- src/IdentityServerSample.ApplicationCore/Repositories/IClientRepository.cs
+++ src/IdentityServerSample.ApplicationCore/Repositories/IClientRepository.cs
@@ -2,16 +2,16 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
-using IdentityServerSample.ApplicationCore.Entities;
-
 namespace IdentityServerSample.ApplicationCore.Repositories
 {
+  using IdentityServerSample.ApplicationCore.Entities;
+
   /// <summary>Provides a simple API to clients in a database.</summary>
   public interface IClientRepository
   {
     /// <summary>Gets a collection of active clients.</summary>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
-    public Task<IEnumerable<ClientEntity>> GetClientAsync(CancellationToken cancellationToken);
+    public Task<ClientEntity[]> GetClientAsync(CancellationToken cancellationToken);
   }
 }
---
+++ src/IdentityServerSample.Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.Infrastructure.Repositories
+{
+  using System;
+
+  using Microsoft.EntityFrameworkCore;
+
+  using IdentityServerSample.ApplicationCore.Entities;
+  using IdentityServerSample.ApplicationCore.Repositories;
+
+  /// <summary>Provides a simple API to clients in a database.</summary>
+  public sealed class ClientRepository : IClientRepository
+  {
+    private readonly DbContext _dbContext;
+
+    /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.Infrastructure.Repositories.ClientRepository"/> class.</summary>
+    /// <param name="dbContext">An object that represents a session with the database and can be used to query and save instances of your entities.</param>
+    public ClientRepository(DbContext dbContext)
+    {
+      _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    /// <summary>Gets a collection of active clients.</summary>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public Task<ClientEntity[]> GetClientAsync(CancellationToken cancellationToken)
+    {
+      return _dbContext.Set<ClientEntity>().ToArrayAsync(cancellationToken);
+    }
+  }
+}
