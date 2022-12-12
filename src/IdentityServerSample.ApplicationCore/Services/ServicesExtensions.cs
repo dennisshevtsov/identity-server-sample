22c8702118a79a8eaab8554d9dd423b71eb61682
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-12 23:16:48 +0300

    imple client srv

---
+++ src/IdentityServerSample.ApplicationCore/Services/ServicesExtensions.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+  using IdentityServerSample.ApplicationCore.Services;
+
+  /// <summary>Provides a simple API to register application services.</summary>
+  public static class ServicesExtensions
+    {
+        /// <summary>Registers application services.</summary>
+        /// <param name="services">An object that specifies the contract for a collection of service descriptors.</param>
+        /// <returns>An object that specifies the contract for a collection of service descriptors.</returns>
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IClientService, ClientService>();
+
+            return services;
+        }
+    }
+}
