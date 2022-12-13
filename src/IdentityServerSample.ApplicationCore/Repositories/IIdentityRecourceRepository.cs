af33bbd351a2630b11f30f40003190215541a638
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-13 13:14:39 +0300

    add id res repo

---
+++ src/IdentityServerSample.ApplicationCore/Repositories/IIdentityRecourceRepository.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.ApplicationCore.Repositories
+{
+  using IdentityServerSample.ApplicationCore.Entities;
+
+  /// <summary>Provides a simple API to query and save identity resouces in the database.</summary>
+  public interface IIdentityRecourceRepository
+  {
+    /// <summary>Gets a collection of all identity resources.</summary>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public Task<IdentityRecourceEntity[]> GetIdentityRecourcesAsync(CancellationToken cancellationToken);
+  }
+}
