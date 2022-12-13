859f5fb43886735f1d3344d847f52ab742cd30a1
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-13 13:07:55 +0300

    add id res ent

---
+++ src/IdentityServerSample.ApplicationCore/Entities/IdentityRecourceEntity.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.ApplicationCore.Entities
+{
+  /// <summary>Represents details of an identity resource.</summary>
+  public sealed class IdentityRecourceEntity
+  {
+    /// <summary>Gets/sets an object that represents a name of an identity resource.</summary>
+    public string? Name { get; set; }
+
+    /// <summary>Gets/sets an object that represents a display name of an identity resource.</summary>
+    public string? DisplayName { get; set; }
+
+    /// <summary>Gets/sets an object that represents a description of an identity resource.</summary>
+    public string? Description { get; set; }
+  }
+}
