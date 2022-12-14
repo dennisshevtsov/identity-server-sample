61295dd26fcbce0ffd1f78ba08a8b40155488f21
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-14 13:05:02 +0300

    impl api res repo

---
+++ src/IdentityServerSample.Infrastructure/Repositories/ApiResourceRepository.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.Infrastructure.Repositories
+{
+  using System;
+
+  using Microsoft.EntityFrameworkCore;
+
+  using IdentityServerSample.ApplicationCore.Entities;
+  using IdentityServerSample.ApplicationCore.Repositories;
+
+  /// <summary>Provides a simple API to query and save API resources.</summary>
+  public sealed class ApiResourceRepository : IApiResourceRepository
+  {
+    private readonly DbContext _dbContext;
+
+    /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.Infrastructure.Repositories.ApiResourceRepository"/> class.</summary>
+    /// <param name="dbContext">An object that represents a session with the database and can be used to query and save instances of your entities.</param>
+    public ApiResourceRepository(DbContext dbContext)
+    {
+      _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    /// <summary>Gets a collection of all API resources.</summary>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public Task<ApiRecourceEntity[]> GetApiResourcesAsync(CancellationToken cancellationToken)
+    {
+      return _dbContext.Set<ApiRecourceEntity>()
+                       .ToArrayAsync(cancellationToken);
+    }
+  }
+}
