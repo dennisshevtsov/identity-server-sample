ca3041c39e1d4c89187619a210c86966ac2858af
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-15 12:50:53 +0300

    add scope srv

---
+++ src/IdentityServerSample.ApplicationCore/Services/IScopeService.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.ApplicationCore.Services
+{
+  using IdentityServerSample.ApplicationCore.Entities;
+
+  /// <summary>Provides a simple API to query and save scopes.</summary>
+  public interface IScopeService
+  {
+    /// <summary>Gets a collection of available scopes.</summary>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public Task<ScopeEntity[]> GetScopesAsync(CancellationToken cancellationToken);
+  }
+}
