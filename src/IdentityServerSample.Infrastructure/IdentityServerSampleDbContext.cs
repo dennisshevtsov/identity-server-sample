89d950165eae883ca146bc6ceca57f7b27674070
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-18 11:55:07 +0300

    add db context

---
+++ src/IdentityServerSample.Infrastructure/IdentityServerSampleDbContext.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.Infrastructure
+{
+  using Microsoft.EntityFrameworkCore;
+
+  using IdentityServerSample.Infrastructure.Configurations;
+
+  public sealed class IdentityServerSampleDbContext : DbContext
+  {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.ApplyConfiguration(new AudienceEntityTypeConfiguration("audiences"));
+      modelBuilder.ApplyConfiguration(new ClientEntityTypeConfiguration("clients"));
+      modelBuilder.ApplyConfiguration(new ScopeEntityTypeConfiguration("scopes"));
+    }
+  }
+}
