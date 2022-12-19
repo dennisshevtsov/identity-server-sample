598cd45f74433609b54c90411a6368869dbc4bf6
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-19 13:57:28 +0300

    impl scope dto

--- src/IdentityServerSample.ApplicationCore/Dtos/GetScopesResponseDto.cs
+++ src/IdentityServerSample.ApplicationCore/Dtos/GetScopesResponseDto.cs
@@ -2,9 +2,24 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using IdentityServerSample.ApplicationCore.Identities;
+
 namespace IdentityServerSample.ApplicationCore.Dtos
 {
+  /// <summary>Represents a response to a get scopes request.</summary>
   public sealed class GetScopesResponseDto
   {
+    /// <summary>Gets/sets a collection of scopes.</summary>
+    public ScopeDto[]? Scopes { get; set; }
+
+    /// <summary>Represents a scope.</summary>
+    public sealed class ScopeDto : IScopeIdentity
+    {
+      /// <summary>Gets/sets an object that represents a name of a scope.</summary>
+      public string? Name { get; set; }
+
+      /// <summary>Gets/sets an object that represents a diaplay name of a scope.</summary>
+      public string? DisplayName { get; set; }
+    }
   }
 }
