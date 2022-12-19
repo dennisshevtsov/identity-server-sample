20607573cc16d0f4f06391dced97134fa43c719a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-19 14:11:47 +0300

    impl get clients dto

--- src/IdentityServerSample.ApplicationCore/Dtos/GetClientsResponseDto.cs
+++ src/IdentityServerSample.ApplicationCore/Dtos/GetClientsResponseDto.cs
@@ -4,7 +4,21 @@
 
 namespace IdentityServerSample.ApplicationCore.Dtos
 {
+  using IdentityServerSample.ApplicationCore.Identities;
+
+  /// <summary>Represents a response to the get clients request.</summary>
   public sealed class GetClientsResponseDto
   {
+    /// <summary>Gets/sets an object that represents a collection of clients.</summary>
+    public ClientDto[]? Clients { get; set; }
+
+    public sealed class ClientDto : IClientIdentity
+    {
+      /// <summary>Gets/sets an object that reprsents an ID of a client.</summary>
+      public string? ClientId { get; set; }
+
+      /// <summary>Gets/sets an object that repesents a display name of a client.</summary>
+      public string? DisplayName { get; set; }
+    }
   }
 }
