1e176a9d6b207436753d95e2017576c085b26742
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-19 14:12:42 +0300

    add client mapping

---
+++ src/IdentityServerSample.ApplicationCore/Mapping/ClientMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.ApplicationCore.Mapping
+{
+  using AutoMapper;
+
+  using IdentityServerSample.ApplicationCore.Dtos;
+  using IdentityServerSample.ApplicationCore.Entities;
+
+  /// <summary>Provides a named configuration for maps.</summary>
+  public sealed class ClientMappingProfile : Profile
+  {
+    /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.ApplicationCore.Mapping.ClientMappingProfile"/> class.</summary>
+    public ClientMappingProfile()
+    {
+      ClientMappingProfile.ConfigureGetClientsMapping(this);
+    }
+
+    private static void ConfigureGetClientsMapping(IProfileExpression expression)
+    {
+      expression.CreateMap<ClientEntity[], GetClientsResponseDto>()
+                .ForMember(dst => dst.Clients, opt => opt.MapFrom(src => src));
+      expression.CreateMap<ClientEntity, GetClientsResponseDto.ClientDto>();
+    }
+  }
+}
