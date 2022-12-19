96012265aeb5390bacff2783e47a0fb7248d98b5
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-19 14:06:24 +0300

    impl get clients

--- src/IdentityServerSample.ApplicationCore/Services/ClientService.cs
+++ src/IdentityServerSample.ApplicationCore/Services/ClientService.cs
@@ -5,30 +5,39 @@
 namespace IdentityServerSample.ApplicationCore.Services
 {
   using System;
-  
+
+  using AutoMapper;
+
   using IdentityServerSample.ApplicationCore.Dtos;
-  using IdentityServerSample.ApplicationCore.Entities;
   using IdentityServerSample.ApplicationCore.Repositories;
 
   /// <summary>Provides a simple API to execute queries and commands with clients.</summary>
   public sealed class ClientService : IClientService
   {
+    private readonly IMapper _mapper;
     private readonly IClientRepository _clientRepository;
 
     /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.ApplicationCore.Services.ClientService"/> class.</summary>
     /// <param name="clientRepository">An object provides a simple API to clients in a database.</param>
-    public ClientService(IClientRepository clientRepository)
+    /// <param name="mapper">An object that provides a simple API to map objects of different types.</param>
+    public ClientService(IClientRepository clientRepository, IMapper mapper)
     {
       _clientRepository = clientRepository ?? throw new ArgumentNullException(nameof(clientRepository));
+      _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
     }
 
     /// <summary>Gets clients that satisfied defined conditions.</summary>
     /// <param name="query">An object that represents conditions to query clients.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
-    public Task<GetClientsResponseDto> GetClientsAsync(GetClientsRequestDto query, CancellationToken cancellationToken)
+    public async Task<GetClientsResponseDto> GetClientsAsync(GetClientsRequestDto query, CancellationToken cancellationToken)
     {
-      return Task.FromResult(new GetClientsResponseDto());
+      var clientEntityCollection =
+        await _clientRepository.GetClientsAsync(cancellationToken);
+      var getClientsResponseDtoCollection =
+        _mapper.Map<GetClientsResponseDto>(clientEntityCollection);
+
+      return getClientsResponseDtoCollection;
     }
   }
 }
--- src/IdentityServerSample.Infrastructure/Repositories/ClientRepository.cs
+++ src/IdentityServerSample.Infrastructure/Repositories/ClientRepository.cs
@@ -37,7 +37,7 @@ namespace IdentityServerSample.Infrastructure.Repositories
     /// <summary>Gets a collection of active clients.</summary>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
-    public Task<ClientEntity[]> GetClientAsync(CancellationToken cancellationToken)
+    public Task<ClientEntity[]> GetClientsAsync(CancellationToken cancellationToken)
     {
       return _dbContext.Set<ClientEntity>().ToArrayAsync(cancellationToken);
     }
