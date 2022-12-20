f1d34f167900b11b8768d2e0c07a9b5c1dd0609d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-20 12:26:52 +0300

    start dbcontext testg

---
+++ test/IdentityServerSample.Test/Integration/Infrastructure/DbContextTest.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.Test.Integration.Infrastructure
+{
+  using Microsoft.EntityFrameworkCore;
+  using Microsoft.Extensions.Configuration;
+  using Microsoft.Extensions.DependencyInjection;
+
+  [TestClass]
+  public sealed class DbContextTest
+  {
+#pragma warning disable CS8618
+    private IDisposable _disposable;
+
+    private DbContext _dbContext;
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                                                    .Build();
+
+      var scope = new ServiceCollection().AddDatabase(configuration)
+                                         .BuildServiceProvider()
+                                         .CreateScope();
+
+      _disposable = scope;
+
+      _dbContext = scope.ServiceProvider.GetRequiredService<DbContext>();
+      _dbContext.Database.EnsureCreated();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+      _dbContext?.Database?.EnsureDeleted();
+      _disposable?.Dispose();
+    }
+  }
+}
