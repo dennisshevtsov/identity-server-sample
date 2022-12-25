a13123793d7c5cfb696a0fea93f1aabea637914a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-25 14:07:49 +0300

    add comments for aud contr

--- src/IdentityServerSample.IdentityApp/Controllers/AudienceController.cs
+++ src/IdentityServerSample.IdentityApp/Controllers/AudienceController.cs
@@ -15,11 +15,15 @@ namespace IdentityServerSample.IdentityApp.Controllers
   [Produces(ContentType.Json)]
   public sealed class AudienceController : ControllerBase
   {
+    /// <summary>Handles the get audiences query request.</summary>
+    /// <param name="query">An oject that represents conditions to query audiences.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
     [HttpGet(Name = nameof(AudienceController.GetResources))]
     [ProducesResponseType(typeof(GetAudiencesResponseDto), StatusCodes.Status200OK)]
-    public IActionResult GetResources(GetAudiencesRequestDto query)
+    public Task<IActionResult> GetResources(GetAudiencesRequestDto query, CancellationToken cancellationToken)
     {
-      return Ok();
+      return Task.FromResult<IActionResult>(Ok());
     }
   }
 }
