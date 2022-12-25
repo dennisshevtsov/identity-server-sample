9e71819b1a5c076ff1291c96c1a11dc64b51408d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-25 14:18:51 +0300

    use aud srv

--- src/IdentityServerSample.IdentityApp/Controllers/AudienceController.cs
+++ src/IdentityServerSample.IdentityApp/Controllers/AudienceController.cs
@@ -4,9 +4,12 @@
 
 namespace IdentityServerSample.IdentityApp.Controllers
 {
+  using System;
+
   using Microsoft.AspNetCore.Mvc;
 
   using IdentityServerSample.ApplicationCore.Dtos;
+  using IdentityServerSample.ApplicationCore.Services;
   using IdentityServerSample.IdentityApp.Defaults;
 
   /// <summary>Provides a simple API to handle HTTP requests.</summary>
@@ -15,15 +18,24 @@ namespace IdentityServerSample.IdentityApp.Controllers
   [Produces(ContentType.Json)]
   public sealed class AudienceController : ControllerBase
   {
+    private readonly IAudienceService _audienceService;
+
+    /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.IdentityApp.Controllers.AudienceController"/> class.</summary>
+    /// <param name="audienceService">An object that provides a simple API to execute audience queries and commands.</param>
+    public AudienceController(IAudienceService audienceService)
+    {
+      _audienceService = audienceService ?? throw new ArgumentNullException(nameof(audienceService));
+    }
+
     /// <summary>Handles the get audiences query request.</summary>
     /// <param name="query">An oject that represents conditions to query audiences.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
     [HttpGet(Name = nameof(AudienceController.GetResources))]
     [ProducesResponseType(typeof(GetAudiencesResponseDto), StatusCodes.Status200OK)]
-    public Task<IActionResult> GetResources(GetAudiencesRequestDto query, CancellationToken cancellationToken)
+    public async Task<IActionResult> GetResources(GetAudiencesRequestDto query, CancellationToken cancellationToken)
     {
-      return Task.FromResult<IActionResult>(Ok());
+      return Ok(await _audienceService.GetAudiencesAsync(query, cancellationToken));
     }
   }
 }
