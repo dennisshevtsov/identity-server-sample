58c43dd83c7fa4ba445630bec31d62895e45abf3
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-12-30 11:31:16 +0300

    start api

---
+++ src/IdentityServerSample.WebApp0/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.WebApp0.Controllers
+{
+  using Microsoft.AspNetCore.Mvc;
+
+  /// <summary>Provides a simple API to handle HTTP requests.</summary>
+  [ApiController]
+  [Route("api/account")]
+  [Produces("application/json")]
+  public sealed class AccountController : ControllerBase
+  {
+  }
+}
