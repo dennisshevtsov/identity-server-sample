97eadbee9f9f34a281c8bfc64bd07da936812007
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-10 00:24:49 +0300

    impl search scope vm

--- src/identity-server-sample-web-app/src/app/scope/componets/search-scopes/search-scopes.view-model.ts
+++ src/identity-server-sample-web-app/src/app/scope/componets/search-scopes/search-scopes.view-model.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { map        } from 'rxjs';
+import { Observable } from 'rxjs';
+
+import { ScopeDto     } from '../../dtos/get-scopes-response.dto';
+import { ScopeService } from '../../services';
+
 @Injectable()
-export class SearchScopesViewModel { }
+export class SearchScopesViewModel {
+  private scopesValue: undefined | ScopeDto[];
+
+  public constructor(private readonly service: ScopeService) { }
+
+  public get scopes(): ScopeDto[] {
+    return this.scopesValue ?? [];
+  }
+
+  public initialize(): Observable<void> {
+    return this.service.getScopes().pipe(map(responseDto => {
+                                     this.scopesValue = responseDto.scopes;
+                                   }));
+  }
+}
