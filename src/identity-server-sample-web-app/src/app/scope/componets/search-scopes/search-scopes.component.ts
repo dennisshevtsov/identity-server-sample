c51582b32573fed538618a28cf52ca5a38679131
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-09 13:23:32 +0300

    start impl search scopes comp

--- src/identity-server-sample-web-app/src/app/scope/componets/search-scopes/search-scopes.component.ts
+++ src/identity-server-sample-web-app/src/app/scope/componets/search-scopes/search-scopes.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
+import { SearchScopesViewModel } from './search-scopes.view-model';
+
 @Component({
   selector: 'app-search-scopes',
   templateUrl: './search-scopes.component.html',
+  providers: [SearchScopesViewModel],
 })
-export class SearchScopesComponent { }
+export class SearchScopesComponent {
+  public constructor(public readonly vm: SearchScopesViewModel) { }
+}
