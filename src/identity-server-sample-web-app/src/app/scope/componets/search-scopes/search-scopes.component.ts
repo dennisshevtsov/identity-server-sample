6332d99c0fa20283213e51fe332a48e182f7ae90
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-10 00:25:08 +0300

    imple search scope comp

--- src/identity-server-sample-web-app/src/app/scope/componets/search-scopes/search-scopes.component.ts
+++ src/identity-server-sample-web-app/src/app/scope/componets/search-scopes/search-scopes.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { OnDestroy } from '@angular/core';
+import { OnInit    } from '@angular/core';
+
+import { Subscription } from 'rxjs';
 
 import { SearchScopesViewModel } from './search-scopes.view-model';
 
 @Component({
   templateUrl: './search-scopes.component.html',
-  providers: [SearchScopesViewModel],
+  providers: [
+    SearchScopesViewModel,
+    {
+      provide: Subscription,
+      useFactory: () => new Subscription(),
+    },
+  ],
 })
-export class SearchScopesComponent {
-  public constructor(public readonly vm: SearchScopesViewModel) { }
+export class SearchScopesComponent implements OnInit, OnDestroy {
+  public constructor(
+    public readonly vm: SearchScopesViewModel,
+
+    private readonly sub: Subscription,
+  ) { }
+
+  public ngOnInit(): void {
+    this.sub.add(this.vm.initialize().subscribe());
+  }
+
+  public ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
