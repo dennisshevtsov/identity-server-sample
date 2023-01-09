5dcc0a1f00923fa8efcfbda741e5618cc2f10240
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-09 12:56:16 +0300

    add add scope comp

---
+++ src/identity-server-sample-web-app/src/app/scope/add-scope/add-scope.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-scope',
+  templateUrl: './add-scope.component.html',
+  styleUrls: ['./add-scope.component.scss']
+})
+export class AddScopeComponent {
+
+}
--- src/identity-server-sample-web-app/src/app/scope/scope.module.ts
+++ src/identity-server-sample-web-app/src/app/scope/scope.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchScopesComponent } from './search-scopes/search-scopes.component';
+import { AddScopeComponent } from './add-scope/add-scope.component';
 
 
 
 @NgModule({
   declarations: [
-    SearchScopesComponent
+    SearchScopesComponent,
+    AddScopeComponent
   ],
   imports: [
     CommonModule
