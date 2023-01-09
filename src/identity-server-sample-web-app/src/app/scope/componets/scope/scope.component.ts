431dd058daa7397ca5b91f09d1b191027df4c736
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-09 13:20:32 +0300

    start impl scope comp

--- src/identity-server-sample-web-app/src/app/scope/componets/scope/scope.component.ts
+++ src/identity-server-sample-web-app/src/app/scope/componets/scope/scope.component.ts
@@ -1,7 +1,38 @@
-import { Component } from '@angular/core';
+import { Component    } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Input        } from '@angular/core';
+import { Output       } from '@angular/core';
+
+import { ScopeViewModel } from './scope.view-model';
 
 @Component({
   selector: 'app-scope',
   templateUrl: './scope.component.html',
 })
-export class ScopeComponent { }
+export class ScopeComponent {
+  private readonly okValue: EventEmitter<void>;
+
+  private scopeValue: undefined | ScopeViewModel;
+
+  public constructor() {
+    this.okValue = new EventEmitter<void>();
+  }
+
+  public get scope(): ScopeViewModel {
+    return this.scopeValue ?? (this.scopeValue = new ScopeViewModel());
+  }
+
+  @Input()
+  public set scope(value: ScopeViewModel) {
+    this.scopeValue = value;
+  }
+
+  @Output()
+  public get ok(): EventEmitter<void> {
+    return this.okValue;
+  }
+
+  public save(): void {
+    this.okValue.emit();
+  }
+}
