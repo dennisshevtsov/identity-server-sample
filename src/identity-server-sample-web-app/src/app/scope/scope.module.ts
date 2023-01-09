fe4cfd8d8edd92ed38fcaeef98f58fc63450df15
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-09 12:54:35 +0300

    add search scope component

--- src/identity-server-sample-web-app/src/app/scope/scope.module.ts
+++ src/identity-server-sample-web-app/src/app/scope/scope.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchScopesComponent } from './search-scopes/search-scopes.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SearchScopesComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ src/identity-server-sample-web-app/src/app/scope/search-scopes/search-scopes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-scopes',
+  templateUrl: './search-scopes.component.html',
+  styleUrls: ['./search-scopes.component.scss']
+})
+export class SearchScopesComponent {
+
+}
