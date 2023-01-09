4ac4af45b399a7f233bc553c0d00b2284b1a8bdb
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-09 12:57:04 +0300

    add update scope comp

--- src/identity-server-sample-web-app/src/app/scope/scope.module.ts
+++ src/identity-server-sample-web-app/src/app/scope/scope.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchScopesComponent } from './search-scopes/search-scopes.component';
 import { AddScopeComponent } from './add-scope/add-scope.component';
+import { UpdateScopeComponent } from './update-scope/update-scope.component';
 
 
 
 @NgModule({
   declarations: [
     SearchScopesComponent,
-    AddScopeComponent
+    AddScopeComponent,
+    UpdateScopeComponent
   ],
   imports: [
     CommonModule
---
+++ src/identity-server-sample-web-app/src/app/scope/update-scope/update-scope.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-scope',
+  templateUrl: './update-scope.component.html',
+  styleUrls: ['./update-scope.component.scss']
+})
+export class UpdateScopeComponent {
+
+}
