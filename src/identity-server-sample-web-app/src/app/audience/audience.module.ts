9265f36ad3d21e07c23bf2a1ecd8f720891ae0c7
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-11 00:16:47 +0300

    reg aud comp

--- src/identity-server-sample-web-app/src/app/audience/audience.module.ts
+++ src/identity-server-sample-web-app/src/app/audience/audience.module.ts
@@ -1,8 +1,18 @@
 import { NgModule     } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AudienceComponent        } from './componets';
+import { AddAudienceComponent     } from './componets';
+import { UpdateAudienceComponent  } from './componets';
+import { SearchAudiencesComponent } from './componets';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddAudienceComponent,
+    AudienceComponent,
+    SearchAudiencesComponent,
+    UpdateAudienceComponent,
+  ],
   imports: [CommonModule],
 })
 export class AudienceModule { }
---
+++ src/identity-server-sample-web-app/src/app/audience/componets/index.ts
@@ -0,0 +1,4 @@
+export * from './add-audience/add-audience.component';
+export * from './audience/audience.component';
+export * from './search-audiences/search-audiences.component';
+export * from './update-audience/update-audience.component';
