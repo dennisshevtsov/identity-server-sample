609b2e34b8d66b6a483ac030e935988bb1cb3aa1
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-12 01:20:39 +0300

    impl aud vm

--- src/identity-server-sample-web-app/src/app/audience/componets/search-audiences/search-audiences.view-model.ts
+++ src/identity-server-sample-web-app/src/app/audience/componets/search-audiences/search-audiences.view-model.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { map        } from 'rxjs';
+import { Observable } from 'rxjs';
+
+import { AudienceDto     } from '../../dtos';
+import { AudienceService } from '../../services';
+
 @Injectable()
-export class SearchAudiencesViewModel { }
+export class SearchAudiencesViewModel {
+  private audiencesValue: undefined | AudienceDto[];
+
+  public constructor(private readonly service: AudienceService) { }
+
+  public get audiences(): AudienceDto[] {
+    return this.audiencesValue ?? [];
+  }
+
+  public initialize(): Observable<void> {
+    return this.service.getAudiences().pipe(map(responseDto => {
+      this.audiencesValue = responseDto.audiences;
+    }));
+  }
+}
