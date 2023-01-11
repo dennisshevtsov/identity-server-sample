f2faabbfedee8b86375fb2bb4a4e10a1f8430fe6
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-12 01:32:43 +0300

    add search clients comp

--- src/identity-server-sample-web-app/src/app/client/client.module.ts
+++ src/identity-server-sample-web-app/src/app/client/client.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddClientComponent } from './components/add-client/add-client.component';
 import { UpdateClientComponent } from './components/update-client/update-client.component';
+import { SearchClientsComponent } from './components/search-clients/search-clients.component';
 
 
 
 @NgModule({
   declarations: [
     AddClientComponent,
-    UpdateClientComponent
+    UpdateClientComponent,
+    SearchClientsComponent
   ],
   imports: [
     CommonModule
