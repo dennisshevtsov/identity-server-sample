b2f592e595e003e7adae2f67881f041047fe1dc4
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-12 01:23:32 +0300

    impl search aud comp

--- src/identity-server-sample-web-app/src/app/audience/componets/search-audiences/search-audiences.component.ts
+++ src/identity-server-sample-web-app/src/app/audience/componets/search-audiences/search-audiences.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { OnDestroy } from '@angular/core';
+import { OnInit    } from '@angular/core';
+
+import { Subscription } from 'rxjs';
 
 import { SearchAudiencesViewModel } from './search-audiences.view-model';
 
 @Component({
   templateUrl: './search-audiences.component.html',
-  providers: [SearchAudiencesViewModel],
+  providers: [
+    SearchAudiencesViewModel,
+    {
+      provide: Subscription,
+      useFactory: () => new Subscription(),
+    },
+  ],
 })
-export class SearchAudiencesComponent { }
+export class SearchAudiencesComponent implements OnInit, OnDestroy {
+  public constructor(
+    public readonly vm: SearchAudiencesViewModel,
+
+    private readonly sub: Subscription,
+  ) { }
+
+  public ngOnInit(): void {
+    this.sub.add(this.vm.initialize().subscribe());
+  }
+
+  public ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+}
