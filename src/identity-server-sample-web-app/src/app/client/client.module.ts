d6c3ad2b6635766802604e54d06e3fa3076e6e79
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-01-12 01:31:50 +0300

    add update client comp

--- src/identity-server-sample-web-app/src/app/client/client.module.ts
+++ src/identity-server-sample-web-app/src/app/client/client.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddClientComponent } from './components/add-client/add-client.component';
+import { UpdateClientComponent } from './components/update-client/update-client.component';
 
 
 
 @NgModule({
   declarations: [
-    AddClientComponent
+    AddClientComponent,
+    UpdateClientComponent
   ],
   imports: [
     CommonModule
---
+++ src/identity-server-sample-web-app/src/app/client/components/update-client/update-client.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './update-client.component.html',
+})
+export class UpdateClientComponent { }
