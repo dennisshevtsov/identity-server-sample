23bc3dbce033ab0598822b97253bf8348f210f3a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-14 13:19:57 +0300

    add integration test for scope repository

---
+++ test/IdentityServerSample.Test/Integration/Infrastructure/Repositories/ScopeRepositoryTest.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.Infrastructure.Repositories.Test
+{
+  using Microsoft.EntityFrameworkCore;
+  using Microsoft.Extensions.DependencyInjection;
+
+  using IdentityServerSample.ApplicationCore.Entities;
+  using IdentityServerSample.ApplicationCore.Repositories;
+  using IdentityServerSample.Infrastructure.Test;
+
+  [TestClass]
+  public sealed class ScopeRepositoryTest : DbIntegrationTestBase
+  {
+#pragma warning disable CS8618
+    private IScopeRepository _scopeRepository;
+#pragma warning restore CS8618
+
+    protected override void InitializeInternal()
+    {
+      _scopeRepository = ServiceProvider.GetRequiredService<IScopeRepository>();
+    }
+  }
+}
