24157793519235b31505a3472908516817ee3bde
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-14 13:24:09 +0300

    add integration test for scope repository

--- test/IdentityServerSample.Test/Integration/Infrastructure/Repositories/ScopeRepositoryTest.cs
+++ test/IdentityServerSample.Test/Integration/Infrastructure/Repositories/ScopeRepositoryTest.cs
@@ -22,5 +22,72 @@ namespace IdentityServerSample.Infrastructure.Repositories.Test
     {
       _scopeRepository = ServiceProvider.GetRequiredService<IScopeRepository>();
     }
+
+    [TestMethod]
+    public async Task GetScopesAsync_Should_Return_All_Scopes()
+    {
+      var controlScopeEntityCollection = await CreateNewScopesAsync(10);
+
+      var testScopeEntityCollection =
+        await _scopeRepository.GetScopesAsync(CancellationToken);
+
+      Assert.AreEqual(controlScopeEntityCollection.Length, testScopeEntityCollection.Count);
+
+      for (int i = 0; i < controlScopeEntityCollection.Length; i++)
+      {
+        AreEqual(controlScopeEntityCollection[i], testScopeEntityCollection[i]);
+      }
+
+      AreDetached(testScopeEntityCollection);
+    }
+
+    private async Task<ScopeEntity> CreateNewScopeAsync()
+    {
+      var scopeEntity = new ScopeEntity
+      {
+        Name = Guid.NewGuid().ToString(),
+        DisplayName = Guid.NewGuid().ToString(),
+        Description = Guid.NewGuid().ToString(),
+      };
+
+      var scopeEntityEntry = DbContext.Add(scopeEntity);
+
+      await DbContext.SaveChangesAsync(CancellationToken);
+
+      scopeEntityEntry.State = EntityState.Detached;
+
+      return scopeEntity;
+    }
+
+    private async Task<ScopeEntity[]> CreateNewScopesAsync(int audiences)
+    {
+      var scopeEntityCollection = new List<ScopeEntity>();
+
+      for (int i = 0; i < audiences; i++)
+      {
+        scopeEntityCollection.Add(await CreateNewScopeAsync());
+      }
+
+      return scopeEntityCollection.OrderBy(entity => entity.Name)
+                                  .ToArray();
+    }
+
+    private void AreEqual(ScopeEntity control, ScopeEntity test)
+    {
+      Assert.AreEqual(control.Name, test.Name);
+      Assert.AreEqual(control.DisplayName, test.DisplayName);
+      Assert.AreEqual(control.Description, test.Description);
+    }
+
+    private void IsDetached(ScopeEntity scopeEntity)
+      => Assert.AreEqual(EntityState.Detached, DbContext.Entry(scopeEntity).State);
+
+    private void AreDetached(List<ScopeEntity> scopeEntityCollection)
+    {
+      foreach (var scopeEntity in scopeEntityCollection)
+      {
+        IsDetached(scopeEntity);
+      }
+    }
   }
 }
