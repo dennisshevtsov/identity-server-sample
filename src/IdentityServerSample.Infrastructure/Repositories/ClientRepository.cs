dcfcfdd78204624ee6303575a1db743a6a723123
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-14 13:01:54 +0300

    add order and make notracking for get clients method

--- src/IdentityServerSample.Infrastructure/Repositories/ClientRepository.cs
+++ src/IdentityServerSample.Infrastructure/Repositories/ClientRepository.cs
@@ -40,7 +40,10 @@ namespace IdentityServerSample.Infrastructure.Repositories
     /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
     public Task<ClientEntity[]> GetClientsAsync(CancellationToken cancellationToken)
     {
-      return _dbContext.Set<ClientEntity>().ToArrayAsync(cancellationToken);
+      return _dbContext.Set<ClientEntity>()
+                       .AsNoTracking()
+                       .OrderBy(entity => entity.Name)
+                       .ToArrayAsync(cancellationToken);
     }
   }
 }
