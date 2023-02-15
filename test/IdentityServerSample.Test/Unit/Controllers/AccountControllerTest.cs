62c2b328cf4f42fe31b8911dc0f7a182e27c36df
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-15 14:09:02 +0300

    remove identity controller base

--- test/IdentityServerSample.Test/Unit/Controllers/AccountControllerTest.cs
+++ test/IdentityServerSample.Test/Unit/Controllers/AccountControllerTest.cs
@@ -4,27 +4,75 @@
 
 namespace IdentityServerSample.IdentityApp.Controllers.Test
 {
+  using IdentityServer4.Models;
   using IdentityServer4.Services;
+  using Microsoft.AspNetCore.Authentication;
+  using Microsoft.AspNetCore.Http;
   using Microsoft.AspNetCore.Identity;
+  using Microsoft.Extensions.Logging;
+  using Microsoft.Extensions.Options;
 
   using IdentityServerSample.IdentityApp.Dtos;
-  using IdentityServer4.Models;
 
   [TestClass]
-  public sealed class AccountControllerTest : IdentityControllerTestBase
+  public sealed class AccountControllerTest
   {
 #pragma warning disable CS8618
+    private Mock<UserManager<UserEntity>> _userManagerMock;
+
+    private Mock<SignInManager<UserEntity>> _signInManagerMock;
+
     private Mock<IIdentityServerInteractionService> _identityServerInteractionServiceMock;
 
     private AccountController _accountController;
 #pragma warning restore CS8618
 
-    protected override void InitializeInternal()
+    [TestInitialize]
+    public void Initialize()
     {
+      var signInManagerLoggerMock = new Mock<ILogger<SignInManager<UserEntity>>>();
+
+      var userStoreMock = new Mock<IUserStore<UserEntity>>();
+      var passwordHasherMock = new Mock<IPasswordHasher<UserEntity>>();
+      var userValidatorMock = new Mock<IUserValidator<UserEntity>>();
+      var passwordValidatorMock = new Mock<IPasswordValidator<UserEntity>>();
+      var keyNormalizerMock = new Mock<ILookupNormalizer>();
+      var errorsMock = new Mock<IdentityErrorDescriber>();
+      var servicesMock = new Mock<IServiceProvider>();
+      var contextAccessorMock = new Mock<IHttpContextAccessor>();
+      var claimsFactoryMock = new Mock<IUserClaimsPrincipalFactory<UserEntity>>();
+      var optionsAccessorMock = new Mock<IOptions<IdentityOptions>>();
+      var userManagerLoggerMock = new Mock<ILogger<UserManager<UserEntity>>>();
+      var schemesMock = new Mock<IAuthenticationSchemeProvider>();
+      var confirmationMock = new Mock<IUserConfirmation<UserEntity>>();
+
+      _userManagerMock = new Mock<UserManager<UserEntity>>(
+        userStoreMock.Object,
+        optionsAccessorMock.Object,
+        passwordHasherMock.Object,
+        new[] { userValidatorMock.Object }.AsEnumerable(),
+        new[] { passwordValidatorMock.Object }.AsEnumerable(),
+        keyNormalizerMock.Object,
+        errorsMock.Object,
+        servicesMock.Object,
+        userManagerLoggerMock.Object);
+
+      _signInManagerMock = new Mock<SignInManager<UserEntity>>(
+        _userManagerMock.Object,
+        contextAccessorMock.Object,
+        claimsFactoryMock.Object,
+        optionsAccessorMock.Object,
+        signInManagerLoggerMock.Object,
+        schemesMock.Object,
+        confirmationMock.Object);
+
+      _userManagerMock.Reset();
+      _signInManagerMock.Reset();
+
       _identityServerInteractionServiceMock = new Mock<IIdentityServerInteractionService>();
 
       _accountController = new AccountController(
-        SignInManagerMock.Object, _identityServerInteractionServiceMock.Object);
+        _signInManagerMock.Object, _identityServerInteractionServiceMock.Object);
     }
 
     [TestMethod]
@@ -46,9 +94,9 @@ namespace IdentityServerSample.IdentityApp.Controllers.Test
     [TestMethod]
     public async Task SingInAccount_Should_Return_Bad_Request_If_Credentials_Are_Not_Valid()
     {
-      SignInManagerMock.Setup(manager => manager.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
-                       .ReturnsAsync(SignInResult.Failed)
-                       .Verifiable();
+      _signInManagerMock.Setup(manager => manager.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>()))
+                        .ReturnsAsync(SignInResult.Failed)
+                        .Verifiable();
 
       var requestDto = new SingInAccountRequestDto();
 
@@ -66,9 +114,9 @@ namespace IdentityServerSample.IdentityApp.Controllers.Test
     [TestMethod]
     public async Task SingOutAccount_Should_Return_Redirect_Result()
     {
-      SignInManagerMock.Setup(manager => manager.SignOutAsync())
-                       .Returns(Task.CompletedTask)
-                       .Verifiable();
+      _signInManagerMock.Setup(manager => manager.SignOutAsync())
+                        .Returns(Task.CompletedTask)
+                        .Verifiable();
 
       var logoutRequest = new LogoutRequest(null, null)
       {
