e614d9591821342a8d1a8e88633510ee3551a29f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-15 13:26:02 +0300

    add unit test for account controller

---
+++ test/IdentityServerSample.Test/Unit/Controllers/AccountControllerTest.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.IdentityApp.Controllers.Test
+{
+  using IdentityServer4.Services;
+
+  [TestClass]
+  public sealed class AccountControllerTest : IdentityControllerTestBase
+  {
+#pragma warning disable CS8618
+    private Mock<IIdentityServerInteractionService> _identityServerInteractionServiceMock;
+
+    private AccountController _accountController;
+#pragma warning restore CS8618
+
+    protected override void InitializeInternal()
+    {
+      _identityServerInteractionServiceMock = new Mock<IIdentityServerInteractionService>();
+
+      _accountController = new AccountController(
+        SignInManagerMock.Object, _identityServerInteractionServiceMock.Object);
+    }
+  }
+}
