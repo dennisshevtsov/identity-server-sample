df6a1174a6d1969dc49e0e957be10d04c4fb8201
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-16 18:22:04 +0300

    add comments for identity extensions

--- src/IdentityServerSample.ApplicationCore/Extensions/IdentityExtensions.cs
+++ src/IdentityServerSample.ApplicationCore/Extensions/IdentityExtensions.cs
@@ -9,9 +9,15 @@ namespace IdentityServerSample.ApplicationCore.Extensions
   /// <summary>Extends identities with additional methods.</summary>
   public static class IdentityExtensions
   {
+    /// <summary>Converts an instance of <see cref="System.Guid"/> to an instance of the <see cref="IdentityServerSample.ApplicationCore.Identities.IUserIdentity"/>.</summary>
+    /// <param name="userId">An object that represents a user ID.</param>
+    /// <returns>An object that represents an identity of a user.</returns>
     public static IUserIdentity? ToUserIdentity(this Guid userId)
       => new UserIdentity(userId);
 
+    /// <summary>Converts an instance of <see cref="System.String"/> to an instance of the <see cref="IdentityServerSample.ApplicationCore.Identities.IUserIdentity"/>.</summary>
+    /// <param name="userId">An object that represents a user ID.</param>
+    /// <returns>An object that represents an identity of a user.</returns>
     public static IUserIdentity? ToUserIdentity(this string userId)
     {
       if (Guid.TryParse(userId, out var userIdGuid))
