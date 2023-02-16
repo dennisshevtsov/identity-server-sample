ee1633258db80c097cc7f0b3657c69b49eaac408
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-16 17:33:13 +0300

    implemet user email store

--- src/IdentityServerSample.IdentityApi/Identity/UserStore.cs
+++ src/IdentityServerSample.IdentityApi/Identity/UserStore.cs
@@ -9,7 +9,7 @@ namespace IdentityServerSample.IdentityApi.Identity
   using IdentityServerSample.ApplicationCore.Entities;
 
   /// <summary>Provides an abstraction for a store which manages user accounts.</summary>
-  public sealed class UserStore : IUserStore<UserEntity>, IUserPasswordStore<UserEntity>, IUserRoleStore<UserEntity>
+  public sealed class UserStore : IUserStore<UserEntity>, IUserPasswordStore<UserEntity>, IUserRoleStore<UserEntity>, IUserEmailStore<UserEntity>
   {
     #region Members of IUserStore
 
@@ -231,6 +231,84 @@ namespace IdentityServerSample.IdentityApi.Identity
     /// The <see cref="Task"/> that represents the asynchronous operation, containing a list of users who are in the named role.
     /// </returns>
     public Task<IList<UserEntity>> GetUsersInRoleAsync(string roleName, CancellationToken cancellationToken)
+      => throw new NotImplementedException();
+
+    #endregion
+
+    #region Members of IUserEmailStore
+
+    /// <summary>
+    /// Sets the <paramref name="email"/> address for a <paramref name="user"/>.
+    /// </summary>
+    /// <param name="user">The user whose email should be set.</param>
+    /// <param name="email">The email to set.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
+    public Task SetEmailAsync(UserEntity user, string? email, CancellationToken cancellationToken)
+      => throw new NotImplementedException();
+
+    /// <summary>
+    /// Gets the email address for the specified <paramref name="user"/>.
+    /// </summary>
+    /// <param name="user">The user whose email should be returned.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
+    /// <returns>The task object containing the results of the asynchronous operation, the email address for the specified <paramref name="user"/>.</returns>
+    public Task<string?> GetEmailAsync(UserEntity user, CancellationToken cancellationToken)
+      => Task.FromResult(user.Email);
+
+    /// <summary>
+    /// Gets a flag indicating whether the email address for the specified <paramref name="user"/> has been verified, true if the email address is verified otherwise
+    /// false.
+    /// </summary>
+    /// <param name="user">The user whose email confirmation status should be returned.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
+    /// <returns>
+    /// The task object containing the results of the asynchronous operation, a flag indicating whether the email address for the specified <paramref name="user"/>
+    /// has been confirmed or not.
+    /// </returns>
+    public Task<bool> GetEmailConfirmedAsync(UserEntity user, CancellationToken cancellationToken)
+      => throw new NotImplementedException();
+
+    /// <summary>
+    /// Sets the flag indicating whether the specified <paramref name="user"/>'s email address has been confirmed or not.
+    /// </summary>
+    /// <param name="user">The user whose email confirmation status should be set.</param>
+    /// <param name="confirmed">A flag indicating if the email address has been confirmed, true if the address is confirmed otherwise false.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
+    public Task SetEmailConfirmedAsync(UserEntity user, bool confirmed, CancellationToken cancellationToken)
+      => throw new NotImplementedException();
+
+    /// <summary>
+    /// Gets the user, if any, associated with the specified, normalized email address.
+    /// </summary>
+    /// <param name="normalizedEmail">The normalized email address to return the user for.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
+    /// <returns>
+    /// The task object containing the results of the asynchronous lookup operation, the user if any associated with the specified normalized email address.
+    /// </returns>
+    public Task<UserEntity?> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken)
+      => throw new NotImplementedException();
+
+    /// <summary>
+    /// Returns the normalized email for the specified <paramref name="user"/>.
+    /// </summary>
+    /// <param name="user">The user whose email address to retrieve.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
+    /// <returns>
+    /// The task object containing the results of the asynchronous lookup operation, the normalized email address if any associated with the specified user.
+    /// </returns>
+    public Task<string?> GetNormalizedEmailAsync(UserEntity user, CancellationToken cancellationToken)
+      => throw new NotImplementedException();
+
+    /// <summary>
+    /// Sets the normalized email for the specified <paramref name="user"/>.
+    /// </summary>
+    /// <param name="user">The user whose email address to set.</param>
+    /// <param name="normalizedEmail">The normalized email to set for the specified <paramref name="user"/>.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
+    public Task SetNormalizedEmailAsync(UserEntity user, string? normalizedEmail, CancellationToken cancellationToken)
       => throw new NotImplementedException();
 
     #endregion
