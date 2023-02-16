2c4bb849f2bc95c54d5fc02f12f476d08cfd7c45
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-16 18:50:10 +0300

    add unit test for identity extension

---
+++ test/IdentityServerSample.Test/Unit/Extensions/IdentityExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.ApplicationCore.Extensions.Test
+{
+  [TestClass]
+  public sealed class IdentityExtensionsTest
+  {
+    [TestMethod]
+    public void ToUserIdentity_Should_Return_User_Identity()
+    {
+      var control = Guid.NewGuid();
+      var test = control.ToUserIdentity();
+
+      Assert.IsNotNull(test);
+      Assert.AreEqual(control, test.UserId);
+    }
+  }
+}
