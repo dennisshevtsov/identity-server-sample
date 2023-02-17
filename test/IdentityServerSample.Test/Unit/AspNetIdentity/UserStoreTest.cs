b151773b40ef5f9c7fb8dedac6b50458dfa6342c
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-17 16:14:05 +0300

    add unit test for user store

---
+++ test/IdentityServerSample.Test/Unit/AspNetIdentity/UserStoreTest.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.IdentityApi.AspNetIdentity.Test
+{
+  [TestClass]
+  public sealed class UserStoreTest
+  {
+    private CancellationToken _cancellationToken;
+
+#pragma warning disable CS8618
+    private Mock<IUserRepository> _userRepositoryMock;
+
+    private UserStore _userStore;
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      _cancellationToken = CancellationToken.None;
+
+      _userRepositoryMock = new Mock<IUserRepository>();
+
+      _userStore = new UserStore(_userRepositoryMock.Object);
+    }
+  }
+}
