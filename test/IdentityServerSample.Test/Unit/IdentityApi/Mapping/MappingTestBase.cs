db57f70ac25f83bbe9a057cc0932109dcee6c3ba
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-18 20:00:24 +0300

    add mapping test base class

---
+++ test/IdentityServerSample.Test/Unit/IdentityApi/Mapping/MappingTestBase.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.IdentityApi.Mapping.Test
+{
+  using Microsoft.Extensions.DependencyInjection;
+
+  public abstract class MappingTestBase
+  {
+#pragma warning disable CS8618
+    private IDisposable _disposable;
+    private IMapper _mapper;
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      var serviceScope = new ServiceCollection().SetUpMapping()
+                                                .BuildServiceProvider()
+                                                .CreateScope();
+
+      _disposable = serviceScope;
+      _mapper = serviceScope.ServiceProvider.GetRequiredService<IMapper>();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+      _disposable?.Dispose();
+    }
+
+    protected IMapper Mapper => _mapper;
+  }
+}
