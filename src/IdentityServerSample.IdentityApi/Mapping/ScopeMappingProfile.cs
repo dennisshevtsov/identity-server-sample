654b8a6fdba77b163221bd2c9dc01d946be22fed
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-18 18:12:29 +0300

    add scope mapping

---
+++ src/IdentityServerSample.IdentityApi/Mapping/ScopeMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.IdentityApi.Mapping
+{
+  using AutoMapper;
+  using IdentityServer4.Models;
+
+  using IdentityServerSample.ApplicationCore.Entities;
+
+  /// <summary>Provides a named configuration for maps.</summary>
+  public sealed class ScopeMappingProfile : Profile
+  {
+    /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.IdentityApi.Mapping.ScopeMappingProfile"/> class.</summary>
+    public ScopeMappingProfile()
+    {
+      ScopeMappingProfile.ConfigureMapping(this);
+    }
+
+    private static void ConfigureMapping(IProfileExpression expression)
+    {
+      expression.CreateMap<ScopeEntity, ApiScope>();
+    }
+  }
+}
