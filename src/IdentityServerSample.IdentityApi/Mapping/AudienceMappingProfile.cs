f55d5a40ba650c0a86f3a9debff39401f0551681
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-18 18:21:56 +0300

    add audience mapping

---
+++ src/IdentityServerSample.IdentityApi/Mapping/AudienceMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.IdentityApi.Mapping
+{
+  using AutoMapper;
+  using IdentityServer4.Models;
+
+  using IdentityServerSample.ApplicationCore.Entities;
+
+  /// <summary>Provides a named configuration for maps.</summary>
+  public sealed class AudienceMappingProfile : Profile
+  {
+    /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.IdentityApi.Mapping.AudienceMappingProfile"/> class.</summary>
+    public AudienceMappingProfile()
+    {
+      AudienceMappingProfile.ConfigureMapping(this);
+    }
+
+    private static void ConfigureMapping(IProfileExpression expression)
+    {
+      expression.CreateMap<AudienceEntity, ApiResource>();
+    }
+  }
+}
