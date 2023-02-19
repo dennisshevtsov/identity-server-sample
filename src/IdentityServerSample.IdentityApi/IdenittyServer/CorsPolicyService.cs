63b4634dcddedb0ff33d5c1f4693d46d7d7b5d63
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-19 17:34:11 +0300

    clean code

--- src/IdentityServerSample.IdentityApi/IdenittyServer/CorsPolicyService.cs
+++ src/IdentityServerSample.IdentityApi/IdenittyServer/CorsPolicyService.cs
@@ -7,21 +7,18 @@ namespace IdentityServerSample.IdentityApi.IdenittyServer
   using System;
 
   using IdentityServer4.Services;
-  using Microsoft.Extensions.Configuration;
 
   using IdentityServerSample.ApplicationCore.Services;
 
   /// <summary>Provides a simple API to determine if CORS is allowed.</summary>
   public sealed class CorsPolicyService : ICorsPolicyService
   {
-    private readonly IConfiguration _configuration;
     private readonly IClientService _clientService;
 
     /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.WebApp.Services.CorsPolicyService"/> class.</summary>
-    /// <param name="configuration">An object that represents a set of key/value application configuration properties.</param>
-    public CorsPolicyService(IConfiguration configuration, IClientService clientService)
+    /// <param name="clientService">An object that provides a simple API to execute queries and commands with clients.</param>
+    public CorsPolicyService(IClientService clientService)
     {
-      _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
       _clientService = clientService ?? throw new ArgumentNullException(nameof(clientService));
     }
 
@@ -30,8 +27,5 @@ namespace IdentityServerSample.IdentityApi.IdenittyServer
     /// <returns>An object that represents an asynchronous operation.</returns>
     public Task<bool> IsOriginAllowedAsync(string origin)
       => _clientService.CheckIfOriginIsAllowedAsync(origin, CancellationToken.None);
-    //{
-    //  return Task.FromResult(origin == _configuration["WebApp_Url"]);
-    //}
   }
 }
