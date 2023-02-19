b434a477e26c0617bb88ec2b1efcff0b7a2115d8
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-19 18:04:26 +0300

    add unit test for cors policy service

---
+++ test/IdentityServerSample.Test/Unit/IdentityApi/IdentityServer/CorsPolicyServiceTest.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.IdentityApi.IdenittyServer.Test
+{
+  using System.Threading;
+
+  [TestClass]
+  public sealed class CorsPolicyServiceTest
+  {
+#pragma warning disable CS8618
+    private Mock<IClientService> _clientServiceMock;
+
+    private CorsPolicyService _corsPolicyService;
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      _clientServiceMock = new Mock<IClientService>();
+
+      _corsPolicyService = new CorsPolicyService(_clientServiceMock.Object);
+    }
+
+    [TestMethod]
+    public async Task IsOriginAllowedAsync_Should_Return_True()
+    {
+      _clientServiceMock.Setup(service => service.CheckIfOriginIsAllowedAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                        .ReturnsAsync(true)
+                        .Verifiable();
+
+      var origin = Guid.NewGuid().ToString();
+      var isAllowed = await _corsPolicyService.IsOriginAllowedAsync(origin);
+
+      Assert.IsTrue(isAllowed);
+
+      _clientServiceMock.Verify(service => service.CheckIfOriginIsAllowedAsync(origin, CancellationToken.None));
+      _clientServiceMock.VerifyNoOtherCalls();
+    }
+  }
+}
