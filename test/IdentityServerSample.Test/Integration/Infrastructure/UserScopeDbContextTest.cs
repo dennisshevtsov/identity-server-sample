0fa2d34143de70b5817647336ad0b1ec5a0fe0f7
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-21 17:52:45 +0300

    add integration test for user scope db context

--- test/IdentityServerSample.Test/Integration/Infrastructure/UserScopeDbContextTest.cs
+++ test/IdentityServerSample.Test/Integration/Infrastructure/UserScopeDbContextTest.cs
@@ -32,6 +32,38 @@ namespace IdentityServerSample.Infrastructure.Test
       Assert.AreEqual(creatingUserScopeEntity.UserId, createdUserScopeEntity.UserId);
     }
 
+    [TestMethod]
+    public async Task SaveChangesAsync_Should_Delete_Scope()
+    {
+      var creatingUserScopeEntity = UserScopeDbContextTest.GenerateTestUserScope();
+
+      var creatingUserScopeEntityEntry = DbContext.Add(creatingUserScopeEntity);
+
+      await DbContext.SaveChangesAsync(CancellationToken);
+
+      creatingUserScopeEntityEntry.State = EntityState.Detached;
+
+      var deletingUserScopeEntity = new UserScopeEntity
+      {
+        Name = creatingUserScopeEntity.Name,
+        UserId= creatingUserScopeEntity.UserId,
+      };
+
+      var deletingUserScopeEntityEntry = DbContext.Remove(deletingUserScopeEntity);
+
+      await DbContext.SaveChangesAsync(CancellationToken);
+
+      deletingUserScopeEntityEntry.State = EntityState.Detached;
+
+      var deletedUserScopeEntity =
+        await DbContext.Set<UserScopeEntity>()
+                       .WithPartitionKey(creatingUserScopeEntity.UserId.ToString())
+                       .Where(entity => entity.Name == creatingUserScopeEntity.Name)
+                       .FirstOrDefaultAsync(CancellationToken);
+
+      Assert.IsNull(deletedUserScopeEntity);
+    }
+
     private static UserScopeEntity GenerateTestUserScope() => new UserScopeEntity
     {
       Name = Guid.NewGuid().ToString(),
