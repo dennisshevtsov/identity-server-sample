a5edccbc95dc67c590f8a6875f47267b5a67235b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-22 12:49:49 +0300

    move identity server code to own project

--- src/IdentityServerSample.IdentityApi/IdenittyServer/IdentityServerExtensions.cs
+++ src/IdentityServerSample.IdentityApi/IdenittyServer/IdentityServerExtensions.cs
@@ -4,7 +4,7 @@
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-  using IdentityServerSample.IdentityApi.IdenittyServer;
+  using IdentityServerSample.IdentityServer;
 
   /// <summary>Provides a simple API to configure a pipeline.</summary>
   public static class IdentityServerExtensions
