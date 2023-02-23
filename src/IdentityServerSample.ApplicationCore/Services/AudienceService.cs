7221b7f0cf558a812d0042ce06e6d234e75334b8
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-23 11:53:10 +0300

    implement get audiences for scopes

--- src/IdentityServerSample.ApplicationCore/Services/AudienceService.cs
+++ src/IdentityServerSample.ApplicationCore/Services/AudienceService.cs
@@ -46,10 +46,14 @@ namespace IdentityServerSample.ApplicationCore.Services
     /// <param name="scopes">An object that represents a collection of scope names.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
-    public Task<List<AudienceEntity>> GetAudiencesAsync(
+    public async Task<List<AudienceEntity>> GetAudiencesAsync(
       IEnumerable<string> scopes, CancellationToken cancellationToken)
     {
-      return Task.FromResult(new List<AudienceEntity>());
+      var audienceEntityCollection =
+        await _audienceRepository.GetAudiencesByScopesAsync(
+          scopes.ToArray(), CancellationToken.None);
+
+      return audienceEntityCollection.ToList();
     }
   }
 }
