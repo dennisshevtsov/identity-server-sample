c44b4a601a98f85c34939efc4808be46d015723b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-23 12:15:56 +0300

    add audience scope repository

---
+++ src/IdentityServerSample.ApplicationCore/Repositories/IAudienceScopeRepository.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.ApplicationCore.Repositories
+{
+  using IdentityServerSample.ApplicationCore.Entities;
+  using IdentityServerSample.ApplicationCore.Identities;
+
+  /// <summary>Provides a simple API to query and save instances of the <see cref="IdentityServerSample.ApplicationCore.Entities.AudienceScopeEntity"/> class.</summary>
+  public interface IAudienceScopeRepository
+  {
+    /// <summary>Gets a collection of the <see cref="IdentityServerSample.ApplicationCore.Entities.AudienceScopeEntity"/> for the <see cref="IdentityServerSample.ApplicationCore.Entities.AudienceEntity"/>.</summary>
+    /// <param name="identity">An object that represents an identity of an audience.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public Task<List<AudienceScopeEntity>> GetScopesAsync(
+      IAudienceIdentity identity, CancellationToken cancellationToken);
+  }
+}
