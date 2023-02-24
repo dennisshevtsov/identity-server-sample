73386438337ea7d5e9a1e619099c934cf8fbaff8
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-24 17:51:46 +0300

    add methods to get audiences to audience service

--- src/IdentityServerSample.ApplicationCore/Services/AudienceService.cs
+++ src/IdentityServerSample.ApplicationCore/Services/AudienceService.cs
@@ -10,6 +10,7 @@ namespace IdentityServerSample.ApplicationCore.Services
 
   using IdentityServerSample.ApplicationCore.Dtos;
   using IdentityServerSample.ApplicationCore.Entities;
+  using IdentityServerSample.ApplicationCore.Identities;
   using IdentityServerSample.ApplicationCore.Repositories;
 
   /// <summary>Provides a simple API to execute audience queries and commands.</summary>
@@ -18,23 +19,25 @@ namespace IdentityServerSample.ApplicationCore.Services
     private readonly IMapper _mapper;
 
     private readonly IAudienceRepository _audienceRepository;
-    private readonly IAudienceScopeRepository _audienceScopeRepository;
+
+    private readonly IAudienceScopeService _audienceScopeService;
 
     /// <summary>Initializes a new instance of the <see cref="IdentityServerSample.ApplicationCore.Services.AudienceService"/> class.</summary>
     /// <param name="mapper">An object that provides a simple API to map objects of different types.</param>
     /// <param name="audienceRepository">An object that provides a simple API to query and save instances of the <see cref="IdentityServerSample.ApplicationCore.Entities.AudienceEntity"/> class.</param>
-    /// <param name="audienceScopeRepository">An object that provides a simple API to query and save instances of the <see cref="IdentityServerSample.ApplicationCore.Entities.AudienceScopeEntity"/> class.</param>
+    /// <param name="audienceScopeService">An object that provides a simple API to execute audience queries and commands.</param>
     public AudienceService(
       IMapper mapper,
       IAudienceRepository audienceRepository,
-      IAudienceScopeRepository audienceScopeRepository)
+      IAudienceScopeService audienceScopeService)
     {
       _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
 
       _audienceRepository = audienceRepository ??
         throw new ArgumentNullException(nameof(audienceRepository));
-      _audienceScopeRepository = audienceScopeRepository ??
-        throw new ArgumentNullException(nameof(audienceScopeRepository));
+
+      _audienceScopeService = audienceScopeService ??
+        throw new ArgumentNullException(nameof(audienceScopeService));
     }
 
     /// <summary>Gets a collection of audiences that satisfy defined conditions.</summary>
@@ -44,51 +47,89 @@ namespace IdentityServerSample.ApplicationCore.Services
     public async Task<GetAudiencesResponseDto> GetAudiencesAsync(
       GetAudiencesRequestDto query, CancellationToken cancellationToken)
     {
-      var audienceEntityCollection = await _audienceRepository.GetAudiencesAsync(cancellationToken);
-      var getAudiencesResponseDto = _mapper.Map<GetAudiencesResponseDto>(audienceEntityCollection);
+      var audienceEntityCollection = await GetAudiencesAsync(cancellationToken);
+      
+      var getAudiencesResponseDto =
+        _mapper.Map<GetAudiencesResponseDto>(audienceEntityCollection);
 
       return getAudiencesResponseDto;
+    }
+
+    /// <summary>Gets a collection of audiences.</summary>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public async Task<List<AudienceEntity>> GetAudiencesAsync(CancellationToken cancellationToken)
+    {
+      var audienceEntityCollection =
+        await _audienceRepository.GetAudiencesAsync(CancellationToken.None);
+
+      var audienceScopeDictionary =
+        await _audienceScopeService.GetAudienceScopesAsync(cancellationToken);
+
+      AudienceService.AddScopes(audienceEntityCollection, audienceScopeDictionary);
+
+      return audienceEntityCollection;
     }
 
     /// <summary>Gets a collection of the <see cref="IdentityServerSample.ApplicationCore.Entities.AudienceEntity"/> that have non-empty intersection with a defined collection of scope names.</summary>
     /// <param name="scopes">An object that represents a collection of scope names.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
-    public async Task<List<AudienceEntity>> GetAudiencesAsync(
+    public async Task<List<AudienceEntity>> GetAudiencesByScopesAsync(
       IEnumerable<string> scopes, CancellationToken cancellationToken)
     {
-      var audienceScopeEntityCollection =
-        await _audienceScopeRepository.GetAudiencesAsync(scopes, cancellationToken);
-
-      var scopesPerAudienceDictionary = new Dictionary<string, List<string>>();
-
-      foreach (var audienceScopeEntity in audienceScopeEntityCollection)
-      {
-        if (!scopesPerAudienceDictionary.TryGetValue(
-          audienceScopeEntity.AudienceName!, out var scopesPerAudience))
-        {
-          scopesPerAudience = new List<string>();
-          scopesPerAudienceDictionary.Add(
-            audienceScopeEntity.AudienceName!, scopesPerAudience);
-        }
-
-        scopesPerAudience.Add(audienceScopeEntity.ScopeName!);
-      }
+      var audienceScopeDictionary =
+        await _audienceScopeService.GetAudienceScopesAsync(
+          scopes.ToScopeIdentities(), cancellationToken);
 
       var audienceEntityCollection =
-        await _audienceRepository.GetAudiencesByNamesAsync(
-          scopesPerAudienceDictionary.Keys, CancellationToken.None);
+        await _audienceRepository.GetAudiencesAsync(
+          audienceScopeDictionary.Keys.ToAudienceIdentities(), CancellationToken.None);
 
-      foreach (var audienceEntity in audienceEntityCollection)
-      {
-        if (scopesPerAudienceDictionary.TryGetValue(
-          audienceEntity.AudienceName!, out var scopesPerAudience))
-        {
-          audienceEntity.Scopes = scopesPerAudience;
-        }
-      }
+      AudienceService.AddScopes(audienceEntityCollection, audienceScopeDictionary);
 
       return audienceEntityCollection;
+    }
+
+    /// <summary>Gets a collection of the <see cref="IdentityServerSample.ApplicationCore.Entities.AudienceEntity"/> with defined names.</summary>
+    /// <param name="audiences">An object that represents a collection of audience names.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public async Task<List<AudienceEntity>> GetAudiencesByNamesAsync(
+      IEnumerable<string> audiences, CancellationToken cancellationToken)
+    {
+      var audienceEntityCollection =
+        await _audienceRepository.GetAudiencesAsync(
+          audiences.ToAudienceIdentities(), CancellationToken.None);
+
+      var audienceScopeDictionary =
+        await _audienceScopeService.GetAudienceScopesAsync(
+          audienceEntityCollection, cancellationToken);
+
+      AudienceService.AddScopes(audienceEntityCollection, audienceScopeDictionary);
+
+      return audienceEntityCollection;
+    }
+
+    private static void AddScopes(
+      AudienceEntity audienceEntity,
+      Dictionary<string, List<string>> audienceScopeDictionary)
+    {
+      if (audienceScopeDictionary.TryGetValue(
+          audienceEntity.AudienceName!, out var scopesPerAudience))
+      {
+        audienceEntity.Scopes = scopesPerAudience;
+      }
+    }
+
+    private static void AddScopes(
+      List<AudienceEntity> audienceEntityCollection,
+      Dictionary<string, List<string>> audienceScopeDictionary)
+    {
+      for (int i = 0; i < audienceEntityCollection.Count; i++)
+      {
+        AudienceService.AddScopes(audienceEntityCollection[i], audienceScopeDictionary);
+      }
     }
   }
 }
