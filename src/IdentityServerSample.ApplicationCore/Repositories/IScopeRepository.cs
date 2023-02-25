5831a2d3cf492c1b2cf46bf0654b52e0be0f4521
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-25 19:22:44 +0300

    add common method to get scopes

--- src/IdentityServerSample.ApplicationCore/Repositories/IScopeRepository.cs
+++ src/IdentityServerSample.ApplicationCore/Repositories/IScopeRepository.cs
@@ -5,19 +5,17 @@
 namespace IdentityServerSample.ApplicationCore.Repositories
 {
   using IdentityServerSample.ApplicationCore.Entities;
+  using IdentityServerSample.ApplicationCore.Identities;
 
   /// <summary>Provides a simple API to query and save instances of the <see cref="IdentityServerSample.ApplicationCore.Entities.ScopeEntity"/> class.</summary>
   public interface IScopeRepository
   {
-    /// <summary>Gets a collection of available scopes.</summary>
-    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
-    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
-    public Task<List<ScopeEntity>> GetScopesAsync(CancellationToken cancellationToken);
-
     /// <summary>Gets a collection of scopes that satisfy defined conditions.</summary>
-    /// <param name="scopes">An object that represents a collection of scope names.</param>
+    /// <param name="identities">An object that represents a collection the <see cref="IdentityServerSample.ApplicationCore.Identities.IScopeIdentity"/>.</param>
+    /// <param name="standard">An object that indicates if scopes are standard.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
-    public Task<List<ScopeEntity>> GetScopesAsync(string[]? scopes, CancellationToken cancellationToken);
+    public Task<List<ScopeEntity>> GetScopesAsync(
+      IEnumerable<IScopeIdentity>? identities, bool standard, CancellationToken cancellationToken);
   }
 }
--- src/IdentityServerSample.Infrastructure/Repositories/ScopeRepository.cs
+++ src/IdentityServerSample.Infrastructure/Repositories/ScopeRepository.cs
@@ -5,10 +5,12 @@
 namespace IdentityServerSample.Infrastructure.Repositories
 {
   using System;
+  using System.Linq;
 
   using Microsoft.EntityFrameworkCore;
 
   using IdentityServerSample.ApplicationCore.Entities;
+  using IdentityServerSample.ApplicationCore.Identities;
   using IdentityServerSample.ApplicationCore.Repositories;
 
   /// <summary>Provides a simple API to query and save instances of the <see cref="IdentityServerSample.ApplicationCore.Entities.ScopeEntity"/> class.</summary>
@@ -23,29 +25,25 @@ namespace IdentityServerSample.Infrastructure.Repositories
       _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
     }
 
-    /// <summary>Gets a collection of available scopes.</summary>
-    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
-    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
-    public Task<List<ScopeEntity>> GetScopesAsync(CancellationToken cancellationToken)
-    {
-      return _dbContext.Set<ScopeEntity>()
-                       .AsNoTracking()
-                       .OrderBy(entity => entity.ScopeName)
-                       .ToListAsync(cancellationToken);
-    }
-
     /// <summary>Gets a collection of scopes that satisfy defined conditions.</summary>
-    /// <param name="scopes">An object that represents a collection of scope names.</param>
+    /// <param name="identities">An object that represents a collection the <see cref="IdentityServerSample.ApplicationCore.Identities.IScopeIdentity"/>.</param>
+    /// <param name="standard">An object that indicates if scopes are standard.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
-    public Task<List<ScopeEntity>> GetScopesAsync(string[]? scopes, CancellationToken cancellationToken)
+    public Task<List<ScopeEntity>> GetScopesAsync(
+      IEnumerable<IScopeIdentity>? identities, bool standard, CancellationToken cancellationToken)
     {
       var dbScopeSet = _dbContext.Set<ScopeEntity>()
-                                 .AsNoTracking();
+                                 .AsNoTracking()
+                                 .Where(entity => entity.Standard == standard);
 
-      if (scopes != null && scopes.Length != 0)
+      if (identities != null && identities.Any())
       {
-        dbScopeSet = dbScopeSet.Where(entity => scopes.Contains(entity.ScopeName));
+        var scopeNameCollection =
+          identities.Select(identity => identity.ScopeName)
+                    .ToList();
+
+        dbScopeSet = dbScopeSet.Where(entity => scopeNameCollection.Contains(entity.ScopeName));
       }
 
       return dbScopeSet.OrderBy(entity => entity.ScopeName)
