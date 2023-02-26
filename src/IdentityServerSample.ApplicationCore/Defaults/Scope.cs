b417d8f5e58896be8c6fcc4d3da62e1306d26543
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-26 11:19:28 +0300

    add constant for default application scope

---
+++ src/IdentityServerSample.ApplicationCore/Defaults/Scope.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.ApplicationCore.Defaults
+{
+  /// <summary>Provides the default values for the application scopes.</summary>
+  public static class Scope
+  {
+    /// <summary>A value that represents a default application scope.</summary>
+    public const string ApplicationScope = "identity-server-sample-scope";
+  }
+}
