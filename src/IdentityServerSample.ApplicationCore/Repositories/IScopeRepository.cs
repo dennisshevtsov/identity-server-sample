01704674f67b8535ce8af60a90518dcddcbe3189
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-26 13:17:46 +0300

    implement scope repository

--- src/IdentityServerSample.ApplicationCore/Repositories/IScopeRepository.cs
+++ src/IdentityServerSample.ApplicationCore/Repositories/IScopeRepository.cs
@@ -20,7 +20,7 @@ namespace IdentityServerSample.ApplicationCore.Repositories
     /// <param name="identity">An object that represents an identity of a scope.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
-    public Task<ScopeEntity> GetScopeAsync(IScopeIdentity identity, CancellationToken cancellationToken);
+    public Task<ScopeEntity?> GetScopeAsync(IScopeIdentity identity, CancellationToken cancellationToken);
 
     /// <summary>Gets a collection of scopes that satisfy defined conditions.</summary>
     /// <param name="identities">An object that represents a collection the <see cref="IdentityServerSample.ApplicationCore.Identities.IScopeIdentity"/>.</param>
