52957b5aaf13bd8abe4e880e127bff1b795fbbeb
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-26 13:32:12 +0300

    add method to add new user

--- src/IdentityServerSample.ApplicationCore/Services/IScopeService.cs
+++ src/IdentityServerSample.ApplicationCore/Services/IScopeService.cs
@@ -38,7 +38,7 @@ namespace IdentityServerSample.ApplicationCore.Services
     /// <param name="identity">An object that represents an identity of a scope.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
     /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
-    public Task<ScopeEntity> GetScopeAsync(
+    public Task<ScopeEntity?> GetScopeAsync(
       IScopeIdentity identity, CancellationToken cancellationToken);
 
     /// <summary>Creates a new scope.</summary>
--- src/IdentityServerSample.ApplicationCore/Services/IUserService.cs
+++ src/IdentityServerSample.ApplicationCore/Services/IUserService.cs
@@ -10,6 +10,12 @@ namespace IdentityServerSample.ApplicationCore.Services
   /// <summary>Provides a simple API to modify/query instances of <see cref="IdentityServerSample.ApplicationCore.Entities.UserEntity"/> class.</summary>
   public interface IUserService
   {
+    /// <summary>Creates a new user.</summary>
+    /// <param name="userEntity">An object that represents details of a user.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public Task AddUserAsync(UserEntity userEntity, CancellationToken cancellationToken);
+
     /// <summary>Gets a user by a user ID.</summary>
     /// <param name="identity">An object that represents an identity of a user.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
