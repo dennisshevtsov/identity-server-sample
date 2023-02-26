a9fe897af35752af47218ae89b151d74db099d9a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-26 13:12:14 +0300

    seed default scope

--- src/IdentityServerSample.ApplicationCore/Services/IScopeService.cs
+++ src/IdentityServerSample.ApplicationCore/Services/IScopeService.cs
@@ -5,6 +5,7 @@
 namespace IdentityServerSample.ApplicationCore.Services
 {
   using IdentityServerSample.ApplicationCore.Entities;
+  using IdentityServerSample.ApplicationCore.Identities;
 
   /// <summary>Provides a simple API to query and save scopes.</summary>
   public interface IScopeService
@@ -32,5 +33,18 @@ namespace IdentityServerSample.ApplicationCore.Services
     /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
     public Task<List<ScopeEntity>> GetStandardScopesAsync(
       IEnumerable<string> scopes, CancellationToken cancellationToken);
+
+    /// <summary>Gets a scope by its identity.</summary>
+    /// <param name="identity">An object that represents an identity of a scope.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public Task<ScopeEntity> GetScopeAsync(
+      IScopeIdentity identity, CancellationToken cancellationToken);
+
+    /// <summary>Creates a new scope.</summary>
+    /// <param name="scopeEntity">An object that represents details of a scope.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public Task AddScopeAsync(ScopeEntity scopeEntity, CancellationToken cancellationToken);
   }
 }
--- src/IdentityServerSample.IdentityApi/Extensions/DatabaseExtensions.cs
+++ src/IdentityServerSample.IdentityApi/Extensions/DatabaseExtensions.cs
@@ -7,6 +7,9 @@ namespace Microsoft.Extensions.DependencyInjection
   using Microsoft.AspNetCore.Identity;
 
   using IdentityServerSample.ApplicationCore.Entities;
+  using IdentityServerSample.ApplicationCore.Services;
+  using IdentityServerSample.ApplicationCore.Defaults;
+  using IdentityServerSample.ApplicationCore.Identities;
 
   /// <summary>Provides methods to extend the API of the <see cref="Microsoft.AspNetCore.Builder.IApplicationBuilder"/>.</summary>
   public static class DatabaseExtensions
@@ -42,18 +45,42 @@ namespace Microsoft.Extensions.DependencyInjection
   {
     private readonly IConfiguration _configuration;
     private readonly UserManager<UserEntity> _userManager;
+    private readonly IScopeService _scopeService;
 
     public DatabaseInitializer(
       IConfiguration configuration,
-      UserManager<UserEntity> userManager)
+      UserManager<UserEntity> userManager,
+      IScopeService scopeService)
     {
       _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
       _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+      _scopeService = scopeService ?? throw new ArgumentNullException(nameof(scopeService));
     }
 
     public async Task ExecuteAsync(CancellationToken cancellationToken)
     {
+      await AddApplicationScopeAsync(cancellationToken);
       await AddTestUserAsync();
+    }
+
+    private async Task AddApplicationScopeAsync(CancellationToken cancellationToken)
+    {
+      var scopeEntity =
+        await _scopeService.GetScopeAsync(
+          Scope.ApplicationScope.ToScopeIdentity(), cancellationToken);
+
+      if (scopeEntity != null)
+      {
+        scopeEntity = new ScopeEntity
+        {
+          ScopeName = Scope.ApplicationScope,
+          Description = "Default Application Scope",
+          DisplayName = "Default Application Scope",
+          Standard = false,
+        };
+
+        await _scopeService.AddScopeAsync(scopeEntity, cancellationToken);
+      }
     }
 
     private async Task AddTestUserAsync()
@@ -72,6 +99,13 @@ namespace Microsoft.Extensions.DependencyInjection
         {
           Email = testUserEmail,
           Name = testUserName,
+          Scopes = new List<UserScopeEntity>
+          {
+            new UserScopeEntity
+            {
+              ScopeName = Scope.ApplicationScope,
+            },
+          },
         };
 
         await _userManager.CreateAsync(testUserEntity, testUserPasword);
