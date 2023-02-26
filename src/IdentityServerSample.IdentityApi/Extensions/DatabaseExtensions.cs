fb919cec0a8d4a151ef35e9385f3aa4b5fdbe082
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-26 11:35:23 +0300

    add database initializer

--- src/IdentityServerSample.IdentityApi/Extensions/DatabaseExtensions.cs
+++ src/IdentityServerSample.IdentityApi/Extensions/DatabaseExtensions.cs
@@ -4,6 +4,10 @@
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+  using Microsoft.AspNetCore.Identity;
+
+  using IdentityServerSample.ApplicationCore.Entities;
+
   /// <summary>Provides methods to extend the API of the <see cref="Microsoft.AspNetCore.Builder.IApplicationBuilder"/>.</summary>
   public static class DatabaseExtensions
   {
@@ -12,9 +16,36 @@ namespace Microsoft.Extensions.DependencyInjection
     /// <returns>An object that defines a class that provides the mechanisms to configure an application's request pipeline.</returns>
     public static IApplicationBuilder SetUpDatabase(this IApplicationBuilder app)
     {
-
+      using (var scope = app.ApplicationServices.CreateScope())
+      {
+        scope.ServiceProvider.GetRequiredService<DatabaseInitializer>()
+                             .ExecuteAsync(CancellationToken.None)
+                             .GetAwaiter()
+                             .GetResult();
+      }
 
       return app;
+    }
+  }
+
+  public sealed class DatabaseInitializer
+  {
+    private UserManager<UserEntity> _userManager;
+
+    public DatabaseInitializer(UserManager<UserEntity> userManager)
+    {
+      _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+    }
+
+    public async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+      var userEntity = new UserEntity
+      {
+        Email = "test@test.test",
+        Name = "Test User",
+      };
+
+      await _userManager.CreateAsync(userEntity, "test");
     }
   }
 }
