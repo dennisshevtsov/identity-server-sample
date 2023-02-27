013413d77a3f3ea1737406e35556981b496b69df
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-28 00:13:26 +0300

    move initialization to own namespase

--- src/IdentityServerSample.IdentityApi/Initialization/DatabaseInitializer.cs
+++ src/IdentityServerSample.IdentityApi/Initialization/DatabaseInitializer.cs
@@ -2,44 +2,12 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace IdentityServerSample.IdentityApi.Initialization
 {
   using Microsoft.AspNetCore.Identity;
 
   using IdentityServerSample.ApplicationCore.Entities;
   using IdentityServerSample.ApplicationCore.Services;
-  using IdentityServerSample.ApplicationCore.Defaults;
-  using IdentityServerSample.ApplicationCore.Identities;
-
-  /// <summary>Provides methods to extend the API of the <see cref="Microsoft.AspNetCore.Builder.IApplicationBuilder"/>.</summary>
-  public static class DatabaseExtensions
-  {
-    /// <summary>Registers the <see cref="Microsoft.Extensions.DependencyInjection.DatabaseInitializer"/>.</summary>
-    /// <param name="services">An object that specifies the contract for a collection of service descriptors.</param>
-    /// <returns>An object that specifies the contract for a collection of service descriptors.</returns>
-    public static IServiceCollection AddDatabaseInitializer(this IServiceCollection services)
-    {
-      services.AddScoped<DatabaseInitializer>();
-
-      return services;
-    }
-
-    /// <summary>Sets up the database with all required data.</summary>
-    /// <param name="app">An object that defines a class that provides the mechanisms to configure an application's request pipeline.</param>
-    /// <returns>An object that defines a class that provides the mechanisms to configure an application's request pipeline.</returns>
-    public static IApplicationBuilder SetUpDatabase(this IApplicationBuilder app)
-    {
-      using (var scope = app.ApplicationServices.CreateScope())
-      {
-        scope.ServiceProvider.GetRequiredService<DatabaseInitializer>()
-                             .ExecuteAsync(CancellationToken.None)
-                             .GetAwaiter()
-                             .GetResult();
-      }
-
-      return app;
-    }
-  }
 
   public sealed class DatabaseInitializer
   {
---
+++ src/IdentityServerSample.IdentityApi/Initialization/InitializationExtensions.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+  using IdentityServerSample.IdentityApi.Initialization;
+
+  /// <summary>Provides methods to initialize the database.</summary>
+  public static class InitializationExtensions
+  {
+    /// <summary>Registers the <see cref="IdentityServerSample.IdentityApi.Initialization.DatabaseInitializer"/>.</summary>
+    /// <param name="services">An object that specifies the contract for a collection of service descriptors.</param>
+    /// <returns>An object that specifies the contract for a collection of service descriptors.</returns>
+    public static IServiceCollection AddDatabaseInitializer(this IServiceCollection services)
+    {
+      services.AddScoped<DatabaseInitializer>();
+
+      return services;
+    }
+
+    /// <summary>Sets up the database with all required data.</summary>
+    /// <param name="app">An object that defines a class that provides the mechanisms to configure an application's request pipeline.</param>
+    /// <returns>An object that defines a class that provides the mechanisms to configure an application's request pipeline.</returns>
+    public static IApplicationBuilder SetUpDatabase(this IApplicationBuilder app)
+    {
+      using (var scope = app.ApplicationServices.CreateScope())
+      {
+        scope.ServiceProvider.GetRequiredService<DatabaseInitializer>()
+                             .ExecuteAsync(CancellationToken.None)
+                             .GetAwaiter()
+                             .GetResult();
+      }
+
+      return app;
+    }
+  }
+}
