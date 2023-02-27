ad49f841d70a02184e7d0f20e5b7c15a5700441d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-02-28 00:35:44 +0300

    add unit test for database initializer

--- test/IdentityServerSample.Test/Unit/IdentityApi/Initialization/DatabaseInitializerTest.cs
+++ test/IdentityServerSample.Test/Unit/IdentityApi/Initialization/DatabaseInitializerTest.cs
@@ -4,14 +4,21 @@
 
 namespace IdentityServerSample.IdentityApi.Initialization.Test
 {
+  using System.Threading;
+
   using Microsoft.AspNetCore.Identity;
   using Microsoft.Extensions.Configuration;
   using Microsoft.Extensions.Logging;
   using Microsoft.Extensions.Options;
 
+  using IdentityServerSample.ApplicationCore.Defaults;
+  using IdentityServerSample.ApplicationCore.Identities;
+
   [TestClass]
   public sealed class DatabaseInitializerTest
   {
+    private CancellationToken _cancellationToken;
+
 #pragma warning disable CS8618
     private Mock<IConfiguration> _configurationMock;
     private Mock<UserManager<UserEntity>> _userManagerMock;
@@ -23,6 +30,10 @@ namespace IdentityServerSample.IdentityApi.Initialization.Test
     [TestInitialize]
     public void Initialize()
     {
+      _cancellationToken = CancellationToken.None;
+
+      _configurationMock = new Mock<IConfiguration>();
+
       var userStoreMock = new Mock<IUserStore<UserEntity>>();
       var passwordHasherMock = new Mock<IPasswordHasher<UserEntity>>();
       var userValidatorMock = new Mock<IUserValidator<UserEntity>>();
@@ -44,12 +55,45 @@ namespace IdentityServerSample.IdentityApi.Initialization.Test
         servicesMock.Object,
         userManagerLoggerMock.Object);
 
+      _scopeServiceMock = new Mock<IScopeService>();
+
       _databaseInitializer = new DatabaseInitializer(
         _configurationMock.Object,
         _userManagerMock.Object,
         _scopeServiceMock.Object);
 
       _userManagerMock.Reset();
+    }
+
+    [TestMethod]
+    public async Task ExecuteAsync_Should_Not_Add_User_And_Scopes()
+    {
+      _scopeServiceMock.Setup(service => service.GetScopeAsync(It.IsAny<IScopeIdentity>(), It.IsAny<CancellationToken>()))
+                       .ReturnsAsync(new ScopeEntity())
+                       .Verifiable();
+
+      _userManagerMock.Setup(manager => manager.FindByNameAsync(It.IsAny<string>()))
+                      .ReturnsAsync(new UserEntity())
+                      .Verifiable();
+
+      var controlToken = Guid.NewGuid().ToString();
+
+      _configurationMock.SetupGet(configuration => configuration[It.IsAny<string>()])
+                        .Returns(controlToken)
+                        .Verifiable();
+
+      await _databaseInitializer.ExecuteAsync(_cancellationToken);
+
+      _scopeServiceMock.Verify(service => service.GetScopeAsync(Scope.ApplicationScope.ToScopeIdentity(), _cancellationToken));
+      _scopeServiceMock.VerifyNoOtherCalls();
+
+      _userManagerMock.Verify(manager => manager.FindByNameAsync(controlToken));
+      _userManagerMock.VerifyNoOtherCalls();
+
+      _configurationMock.VerifyGet(configuration => configuration["TestUser_Email"]);
+      _configurationMock.VerifyGet(configuration => configuration["TestUser_Name"]);
+      _configurationMock.VerifyGet(configuration => configuration["TestUser_Password"]);
+      _configurationMock.VerifyNoOtherCalls();
     }
   }
 }
