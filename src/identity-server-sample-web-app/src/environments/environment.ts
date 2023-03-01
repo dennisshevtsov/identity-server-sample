2c81eed4d08af4a0282af49c9e4024aba5be669f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-01 13:14:27 +0300

    add environment variables

---
+++ src/identity-server-sample-web-app/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  identityApiUrl: 'http://localhost:5188',
+  appUrl: 'http://localhost:4202',
+  clientId: 'identity-server-sample-api-client-id-1',
+  scope: 'identity-server-sample-api-scope',
+};
