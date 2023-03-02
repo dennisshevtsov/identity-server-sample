4495a15aac512c6e955bd80adb60865e10e16fc2
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-02 12:27:05 +0300

    move identity server mapping to identity server project

--- src/IdentityServerSample.IdentityServer/Mapping/ClientMappingProfile.cs
+++ src/IdentityServerSample.IdentityServer/Mapping/ClientMappingProfile.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
-namespace IdentityServerSample.IdentityApi.Mapping
+namespace IdentityServerSample.IdentityServer.Mapping
 {
   using AutoMapper;
   using IdentityServer4.Models;
