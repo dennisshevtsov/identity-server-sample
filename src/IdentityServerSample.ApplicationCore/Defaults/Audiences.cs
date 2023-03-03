74d91b5e067552d4c8e8a0a4601cbb4133dbcbfb
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-03 11:15:02 +0300

    add audience constants

---
+++ src/IdentityServerSample.ApplicationCore/Defaults/Audiences.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.ApplicationCore.Defaults
+{
+  /// <summary>Provides the default values for the application audience.</summary>
+  public static class Audiences
+  {
+    /// <summary>A value that represents a default application audience.</summary>
+    public const string ApplicationAudience = "identity-server-sample-api";
+  }
+}
