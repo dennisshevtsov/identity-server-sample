0e383847b3331b4b739ad754511a0aedd6bac0b4
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-03 11:52:38 +0300

    add get client by name method to client service

--- src/IdentityServerSample.ApplicationCore/Services/ClientService.cs
+++ src/IdentityServerSample.ApplicationCore/Services/ClientService.cs
@@ -34,6 +34,13 @@ namespace IdentityServerSample.ApplicationCore.Services
     public Task AddClientAsync(ClientEntity clientEntity, CancellationToken cancellationToken)
       => _clientRepository.AddClientAsync(clientEntity, cancellationToken);
 
+    /// <summary>Gets a client by its name.</summary>
+    /// <param name="clientName">An object that represents a name of a client.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public Task<ClientEntity?> GetClientAsync(string clientName, CancellationToken cancellationToken)
+      => _clientRepository.GetClientAsync(clientName, cancellationToken);
+
     /// <summary>Gets clients that satisfied defined conditions.</summary>
     /// <param name="query">An object that represents conditions to query clients.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
--- src/IdentityServerSample.ApplicationCore/Services/IClientService.cs
+++ src/IdentityServerSample.ApplicationCore/Services/IClientService.cs
@@ -16,6 +16,12 @@ namespace IdentityServerSample.ApplicationCore.Services
     /// <returns>An object that tepresents an asynchronous operation.</returns>
     public Task AddClientAsync(ClientEntity clientEntity, CancellationToken cancellationToken);
 
+    /// <summary>Gets a client by its name.</summary>
+    /// <param name="clientName">An object that represents a name of a client.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public Task<ClientEntity?> GetClientAsync(string clientName, CancellationToken cancellationToken);
+
     /// <summary>Gets clients that satisfied defined conditions.</summary>
     /// <param name="query">An object that represents conditions to query clients.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
