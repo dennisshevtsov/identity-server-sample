ba3d312515da86b0c27a38a38b9546a8e66945b9
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-04 11:31:31 +0300

    add subscription to add scope component

--- src/identity-server-sample-web-app/src/app/scope/componets/add-scope/add-scope.component.ts
+++ src/identity-server-sample-web-app/src/app/scope/componets/add-scope/add-scope.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { Router    } from '@angular/router';
 
+import { Subscription } from 'rxjs';
+
 import { AddScopeViewModel } from './add-scope.view-model';
 
 @Component({
   templateUrl: './add-scope.component.html',
-  providers: [AddScopeViewModel],
+  providers: [AddScopeViewModel, Subscription],
 })
 export class AddScopeComponent {
   public constructor(
     public readonly vm: AddScopeViewModel,
+
+    private readonly subscription: Subscription,
     private readonly router: Router,
   ) { }
 
