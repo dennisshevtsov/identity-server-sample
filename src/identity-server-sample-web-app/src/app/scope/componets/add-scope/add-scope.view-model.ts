904bcbc9e7c6061baede4798800d3e8c1b5f3953
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-04 11:29:20 +0300

    implement add method in add scope vm

--- src/identity-server-sample-web-app/src/app/scope/componets/add-scope/add-scope.view-model.ts
+++ src/identity-server-sample-web-app/src/app/scope/componets/add-scope/add-scope.view-model.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { AddScopeRequestDto } from '../../dtos';
+import { ScopeService       } from '../../services';
+
 import { ScopeViewModel } from '../scope';
 
 @Injectable()
 export class AddScopeViewModel {
   private scopeValue: undefined | ScopeViewModel;
 
+  public constructor(private readonly service: ScopeService) { }
+
   public get scope(): ScopeViewModel {
     return this.scopeValue ?? (this.scopeValue = new ScopeViewModel());
   }
 
-  public add(): void {}
+  public add(): Observable<void> {
+    const requestDto = new AddScopeRequestDto(
+      this.scope.scopeName,
+      this.scope.displayName,
+    );
+
+    return this.service.addScope(requestDto);
+  }
 }
