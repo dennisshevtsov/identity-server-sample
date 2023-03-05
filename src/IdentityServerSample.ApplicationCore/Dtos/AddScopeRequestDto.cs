50dd9e2718c94c9dc96e88dbc17e8338778bc58b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-05 16:49:48 +0300

    add add scope request dto

---
+++ src/IdentityServerSample.ApplicationCore/Dtos/AddScopeRequestDto.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.ApplicationCore.Dtos
+{
+  using IdentityServerSample.ApplicationCore.Identities;
+
+  /// <summary>Represents data to create a new scope.</summary>
+  public sealed class AddScopeRequestDto : IScopeIdentity
+  {
+    /// <summary>Gets/sets an object that represents a name of a scope.</summary>
+    public string? ScopeName { get; set; }
+  }
+}
