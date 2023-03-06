f88ba460954242dadb8ba9f0698f358c446bc5ee
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-06 23:27:58 +0300

    add method to get client in client service

--- src/IdentityServerSample.ApplicationCore/Services/IClientService.cs
+++ src/IdentityServerSample.ApplicationCore/Services/IClientService.cs
@@ -16,6 +16,12 @@ namespace IdentityServerSample.ApplicationCore.Services
     /// <returns>An object that tepresents an asynchronous operation.</returns>
     public Task AddClientAsync(ClientEntity clientEntity, CancellationToken cancellationToken);
 
+    /// <summary>Gets a client that satisfy defined conditions.</summary>
+    /// <param name="requestDto">An object that represents conditions to query a client.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that tepresents an asynchronous operation that produces a result at some time in the future.</returns>
+    public Task<ClientEntity?> GetClientAsync(GetClientRequestDto requestDto, CancellationToken cancellationToken);
+
     /// <summary>Gets a client by its name.</summary>
     /// <param name="clientName">An object that represents a name of a client.</param>
     /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
