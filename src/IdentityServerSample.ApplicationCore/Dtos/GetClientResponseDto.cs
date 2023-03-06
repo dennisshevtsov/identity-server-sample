001191230d1ca50218dcde16b99cec26ae854b16
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-06 23:19:40 +0300

    add get client dtos

---
+++ src/IdentityServerSample.ApplicationCore/Dtos/GetClientResponseDto.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.ApplicationCore.Dtos
+{
+  using IdentityServerSample.ApplicationCore.Identities;
+
+  public sealed class GetClientResponseDto : IClientIdentity
+  {
+    /// <summary>Gets/sets an object that reprsents a name of a client.</summary>
+    public string? ClientName { get; set; }
+
+    /// <summary>Gets/sets an object that repesents a display name of a client.</summary>
+    public string? DisplayName { get; set; }
+
+    /// <summary>Gets/sets an object that repesents a desciption of a client.</summary>
+    public string? Desciption { get; set; }
+  }
+}
