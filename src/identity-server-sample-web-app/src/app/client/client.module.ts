f79983c3953f3150f4accf9cf57711dc313148d3
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-08 12:49:58 +0300

    add client component

--- src/identity-server-sample-web-app/src/app/client/client.module.ts
+++ src/identity-server-sample-web-app/src/app/client/client.module.ts
@@ -2,6 +2,7 @@ import { NgModule     } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AddClientComponent     } from './components';
+import { ClientComponent        } from './components';
 import { SearchClientsComponent } from './components';
 
 import { ClientRoutingModule } from './client-routing.module';
@@ -9,6 +10,7 @@ import { ClientRoutingModule } from './client-routing.module';
 @NgModule({
   declarations: [
     AddClientComponent,
+    ClientComponent,
     SearchClientsComponent,
   ],
   imports: [
