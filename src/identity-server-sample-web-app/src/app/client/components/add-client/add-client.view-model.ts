b5526f5e1feefd895674a632d91cc67ec98a3219
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-09 01:29:48 +0300

    use add client method

--- src/identity-server-sample-web-app/src/app/client/components/add-client/add-client.view-model.ts
+++ src/identity-server-sample-web-app/src/app/client/components/add-client/add-client.view-model.ts
@@ -1,16 +1,29 @@
 import { Observable } from 'rxjs';
-import { of         } from 'rxjs';
 
-import { ClientViewModel } from '../client/client.view-model';
+import { AddClientRequestDto } from '../../dtos/add-client-request.dto';
+import { ClientService       } from '../../services/client.service';
+import { ClientViewModel     } from '../client/client.view-model';
 
 export class AddClientViewModel {
   private clientValue: undefined | ClientViewModel;
+
+  public constructor(public service: ClientService) {}
 
   public get client(): ClientViewModel {
     return this.clientValue ?? (this.clientValue = new ClientViewModel());
   }
 
   public add(): Observable<void> {
-    return of(void 0);
+    const requestDto = new AddClientRequestDto(
+      this.client.clientName,
+      this.client.displayName,
+      this.client.description,
+      ['test'],
+      ['test'],
+      ['test'],
+      ['test'],
+    );
+
+    return this.service.addClient(requestDto);
   }
 }
