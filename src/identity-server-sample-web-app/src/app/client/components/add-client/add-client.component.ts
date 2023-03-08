20bda1a73b19b8330220a659caddc068a119d79d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-08 12:45:14 +0300

    clean add client component

--- src/identity-server-sample-web-app/src/app/client/components/add-client/add-client.component.ts
+++ src/identity-server-sample-web-app/src/app/client/components/add-client/add-client.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-add-client',
   templateUrl: './add-client.component.html',
-  styleUrls: ['./add-client.component.scss']
 })
-export class AddClientComponent {
-
-}
+export class AddClientComponent { }
