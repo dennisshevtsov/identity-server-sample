48fde5f1a5a862812efd49d7ee3164fe4f603eba
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-08 13:31:28 +0300

    build form for client component

--- src/identity-server-sample-web-app/src/app/client/components/client/client.component.ts
+++ src/identity-server-sample-web-app/src/app/client/components/client/client.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
 import { Input     } from '@angular/core';
 
+import { FormArray   } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { FormGroup   } from '@angular/forms';
+import { Validators  } from '@angular/forms';
+
 import { ClientViewModel } from './client.view-model';
+
+type ClientFormScheme = {
+  [K in keyof ClientViewModel]: ClientViewModel[K] extends Array<infer T> ?
+                                FormArray<FormControl<T | null>> :
+                                FormControl<ClientViewModel[K] | null>;
+};
 
 @Component({
   selector: 'app-client',
@@ -9,9 +21,28 @@ import { ClientViewModel } from './client.view-model';
 })
 export class ClientComponent {
   private clientValue: undefined | ClientViewModel;
+  private formValue  : undefined | FormGroup<ClientFormScheme>;
+
+  public constructor(private readonly fb: FormBuilder) {}
 
   @Input()
   public set client(value: ClientViewModel) {
     this.clientValue = value;
+  }
+
+  public get form(): FormGroup<ClientFormScheme> {
+    return this.formValue ?? (this.formValue = this.buildForm());
+  }
+
+  private buildForm(): FormGroup<ClientFormScheme> {
+    return this.fb.group({
+      clientName      : this.fb.control('', [Validators.required]),
+      displayName     : this.fb.control(''),
+      description     : this.fb.control(''),
+      scopes          : this.fb.array(new Array<string>()),
+      redirectUris    : this.fb.array(new Array<string>()),
+      postRedirectUris: this.fb.array(new Array<string>()),
+      corsOrigins     : this.fb.array(new Array<string>()),
+    });
   }
 }
