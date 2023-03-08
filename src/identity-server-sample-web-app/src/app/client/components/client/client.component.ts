60f0a885fba66441f9da5491a79c672573f79880
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-08 13:02:27 +0300

    add input to client component

--- src/identity-server-sample-web-app/src/app/client/components/client/client.component.ts
+++ src/identity-server-sample-web-app/src/app/client/components/client/client.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { Input     } from '@angular/core';
+
+import { ClientViewModel } from './client.view-model';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
 })
-export class ClientComponent { }
+export class ClientComponent {
+  private clientValue: undefined | ClientViewModel;
+
+  @Input()
+  public set client(value: ClientViewModel) {
+    this.clientValue = value;
+  }
+}
