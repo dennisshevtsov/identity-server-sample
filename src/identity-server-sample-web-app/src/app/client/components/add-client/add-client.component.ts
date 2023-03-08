6bf45e46f5581c6676091497c97b7a06d9e3d92d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-09 01:08:36 +0300

    use add client view model

--- src/identity-server-sample-web-app/src/app/client/components/add-client/add-client.component.ts
+++ src/identity-server-sample-web-app/src/app/client/components/add-client/add-client.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 
+import { AddClientViewModel } from './add-client.view-model';
+
 @Component({
   templateUrl: './add-client.component.html',
+  providers: [AddClientViewModel],
 })
-export class AddClientComponent { }
+export class AddClientComponent {
+  public constructor(public readonly vm: AddClientViewModel) {}
+}
