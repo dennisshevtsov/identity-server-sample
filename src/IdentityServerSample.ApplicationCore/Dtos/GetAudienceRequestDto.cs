523a40aa858f6f21eef3401f9b1b7f2e2d68b73b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-10 12:27:29 +0300

    add get audience request dto

---
+++ src/IdentityServerSample.ApplicationCore/Dtos/GetAudienceRequestDto.cs
@@ -0,0 +1,14 @@
+﻿// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace IdentityServerSample.ApplicationCore.Dtos
+{
+  using IdentityServerSample.ApplicationCore.Identities;
+
+  /// <summary>Represents conditions to query an audience.</summary>
+  public sealed class GetAudienceRequestDto : IAudienceIdentity
+  {
+    /// <summary>Gets/sets an object that represents an ID of an audience.</summary>
+    public string? AudienceName { get; set; }
+  }
+}
