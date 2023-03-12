28fee0240c3899f7854165d8642c92e76ee13312
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-12 15:55:18 +0300

    clean audience component

--- src/identity-server-sample-web-app/src/app/audience/components/audience/audience.component.ts
+++ src/identity-server-sample-web-app/src/app/audience/components/audience/audience.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-audience',
   templateUrl: './audience.component.html',
-  styleUrls: ['./audience.component.scss']
 })
-export class AudienceComponent {
-
-}
+export class AudienceComponent { }
