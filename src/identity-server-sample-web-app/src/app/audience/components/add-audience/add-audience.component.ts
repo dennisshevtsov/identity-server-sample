c6f06a302373f01088ea74e6455e48c98c6eae80
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-12 16:33:37 +0300

    use add audience view model

--- src/identity-server-sample-web-app/src/app/audience/components/add-audience/add-audience.component.ts
+++ src/identity-server-sample-web-app/src/app/audience/components/add-audience/add-audience.component.ts
@@ -1,6 +1,26 @@
 import { Component } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+
+import { AudienceViewModel    } from '../audience/audience.view-model';
+import { AddAudienceViewModel } from './add-audience.view-model';
+
 @Component({
   templateUrl: './add-audience.component.html',
+  providers: [AddAudienceComponent]
 })
-export class AddAudienceComponent { }
+export class AddAudienceComponent {
+  private readonly sub: Subscription;
+
+  public constructor(private readonly vm: AddAudienceViewModel) {
+    this.sub = new Subscription();
+  }
+
+  public get audience(): AudienceViewModel {
+    return this.vm.audience;
+  }
+
+  public ok(): void {
+    this.sub.add(this.vm.add());
+  }
+}
