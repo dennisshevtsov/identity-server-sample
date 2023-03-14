23a77b0c4a5f3bc1e79048f53da7c7366fd857b9
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-15 00:54:14 +0300

    clean services

--- src/identity-server-sample-web-app/src/app/client/services/client.service.ts
+++ src/identity-server-sample-web-app/src/app/client/services/client.service.ts
@@ -1,11 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { UserManager } from 'oidc-client';
-
-import { from       } from 'rxjs';
 import { Observable } from 'rxjs';
-import { switchMap  } from 'rxjs';
 
 import { AddClientRequestDto    } from '../dtos';
 import { GetClientRequestDto    } from '../dtos';
@@ -14,59 +10,30 @@ import { GetClientsResponseDto  } from '../dtos';
 import { UpdateClientRequestDto } from '../dtos';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClientService {
-  public constructor(
-    private readonly um  : UserManager,
-    private readonly http: HttpClient,
-  ) { }
+  public constructor(private readonly http: HttpClient) {}
 
   public getClients(): Observable<GetClientsResponseDto> {
     return this.http.get<GetClientsResponseDto>('api/client');
   }
 
   public getClient(requestDto: GetClientRequestDto): Observable<GetClientResponseDto> {
-    return from(this.um.getUser()).pipe(switchMap(user => {
-      const url = `api/client/${requestDto.clientName}`;
-      const options = {
-        headers: {
-          'Content-Type' : 'application/json',
-          'Authorization': `Bearer ${user?.access_token}`,
-        },
-      };
-
-      return this.http.get<GetClientResponseDto>(url, options);
-    }));
+    return this.http.get<GetClientResponseDto>(`api/client/${requestDto.clientName}`);
   }
 
   public addClient(requestDto: AddClientRequestDto): Observable<void> {
-    return from(this.um.getUser()).pipe(switchMap(user => {
-      const url = 'api/client';
-      const body = JSON.stringify(requestDto);
-      const options = {
-        headers: {
-          'Content-Type' : 'application/json',
-          'Authorization': `Bearer ${user?.access_token}`,
-        },
-      };
+    const url = 'api/client';
+    const body = JSON.stringify(requestDto);
 
-      return this.http.post<void>(url, body, options);
-    }));
+    return this.http.post<void>(url, body);
   }
 
   public updateClient(requestDto: UpdateClientRequestDto): Observable<void> {
-    return from(this.um.getUser()).pipe(switchMap(user => {
-      const url = `api/client/${requestDto.clientName}`;
-      const body = JSON.stringify(requestDto);
-      const options = {
-        headers: {
-          'Content-Type' : 'application/json',
-          'Authorization': `Bearer ${user?.access_token}`,
-        },
-      };
+    const url = `api/client/${requestDto.clientName}`;
+    const body = JSON.stringify(requestDto);
 
-      return this.http.put<void>(url, body, options);
-    }));
+    return this.http.put<void>(url, body);
   }
 }
--- src/identity-server-sample-web-app/src/app/scope/services/scope.service.ts
+++ src/identity-server-sample-web-app/src/app/scope/services/scope.service.ts
@@ -1,11 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { UserManager } from 'oidc-client';
-
-import { from       } from 'rxjs';
 import { Observable } from 'rxjs';
-import { switchMap  } from 'rxjs';
 
 import { AddScopeRequestDto   } from '../dtos';
 import { GetScopesResponseDto } from '../dtos';
@@ -14,37 +10,16 @@ import { GetScopesResponseDto } from '../dtos';
   providedIn: 'root',
 })
 export class ScopeService {
-  constructor(
-    private readonly um  : UserManager,
-    private readonly http: HttpClient,
-  ) { }
+  constructor(private readonly http: HttpClient) {}
 
   public addScope(requestDto: AddScopeRequestDto): Observable<void> {
-    return from(this.um.getUser()).pipe(switchMap(user => {
-      const url = 'api/scope';
-      const body = JSON.stringify(requestDto);
-      const options = {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${user!.access_token}`,
-        },
-      };
+    const url = 'api/scope';
+    const body = JSON.stringify(requestDto);
 
-      return this.http.post<void>(url, body, options)
-    }));
+    return this.http.post<void>(url, body);
   }
 
   public getScopes(): Observable<GetScopesResponseDto> {
-    return from(this.um.getUser()).pipe(switchMap(user => {
-      const url = 'api/scope';
-      const options = {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${user!.access_token}`,
-        },
-      };
-
-      return this.http.get<GetScopesResponseDto>(url, options)
-    }));
+    return this.http.get<GetScopesResponseDto>('api/scope');
   }
 }
