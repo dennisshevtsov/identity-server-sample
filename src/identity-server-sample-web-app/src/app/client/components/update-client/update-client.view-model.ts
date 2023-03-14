cad5f03384e2e55353ff6e496857b6887b68b5c2
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-14 23:17:51 +0300

    add client property and update method to view model:

--- src/identity-server-sample-web-app/src/app/client/components/update-client/update-client.view-model.ts
+++ src/identity-server-sample-web-app/src/app/client/components/update-client/update-client.view-model.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { of         } from 'rxjs';
+
+import { ClientViewModel } from '../client';
+import { ClientService   } from '../../services';
+
 @Injectable()
 export class UpdateClientViewModel {
+  private clientValue: undefined | ClientViewModel;
 
+  public constructor(public service: ClientService) {}
+
+  public get client(): ClientViewModel {
+    return this.clientValue ?? (this.clientValue = new ClientViewModel());
+  }
+
+  public update(): Observable<void> {
+    return of(void 0);
+  }
 }
