badd6210f47e02593d45bd1583175b0438c8bb74
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-15 00:06:48 +0300

    fix update client request dto

--- src/identity-server-sample-web-app/src/app/client/dtos/update-client-request.dto.ts
+++ src/identity-server-sample-web-app/src/app/client/dtos/update-client-request.dto.ts
@@ -3,9 +3,9 @@ export class UpdateClientRequestDto {
     public readonly clientName      : string,
     public readonly displayName     : string,
     public readonly description     : string,
-    public readonly scopes          : string,
-    public readonly redirectUris    : string,
-    public readonly postRedirectUris: string,
-    public readonly corsOrigins     : string,
+    public readonly scopes          : string[],
+    public readonly redirectUris    : string[],
+    public readonly postRedirectUris: string[],
+    public readonly corsOrigins     : string[],
   ) {}
 }
