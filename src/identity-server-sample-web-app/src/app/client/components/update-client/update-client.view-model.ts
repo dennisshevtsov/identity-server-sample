536864082002b13e02474d225f6c3ee6272b5d7d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-14 23:50:34 +0300

    populate client in update client view model

--- src/identity-server-sample-web-app/src/app/client/components/update-client/update-client.view-model.ts
+++ src/identity-server-sample-web-app/src/app/client/components/update-client/update-client.view-model.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 
+import { map        } from 'rxjs';
 import { Observable } from 'rxjs';
 import { of         } from 'rxjs';
 
-import { ClientViewModel } from '../client';
-import { ClientService   } from '../../services';
+import { ClientViewModel      } from '../client';
+import { ClientService        } from '../../services';
+import { GetClientRequestDto  } from '../../dtos';
+import { GetClientResponseDto } from '../../dtos';
 
 @Injectable()
 export class UpdateClientViewModel {
@@ -17,7 +20,18 @@ export class UpdateClientViewModel {
   }
 
   public initialize(): Observable<void> {
-    return of(void 0);
+    const requestDto = new GetClientRequestDto(this.client.clientName);
+    const fillClient = (responseDto: GetClientResponseDto) => {
+      this.client.displayName      = responseDto.displayName;
+      this.client.description      = responseDto.description;
+      this.client.scopes           = [...responseDto.scopes          ];
+      this.client.redirectUris     = [...responseDto.redirectUris    ];
+      this.client.postRedirectUris = [...responseDto.postRedirectUris];
+      this.client.corsOrigins      = [...responseDto.corsOrigins     ];
+    }
+
+    return this.service.getClient(requestDto)
+                       .pipe(map(fillClient));
   }
 
   public update(): Observable<void> {
