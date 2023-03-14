d68bbd8206ba922ebf5ff3b793edbc9b83880099
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-14 23:25:16 +0300

    use view model in update client component

--- src/identity-server-sample-web-app/src/app/client/components/update-client/update-client.component.ts
+++ src/identity-server-sample-web-app/src/app/client/components/update-client/update-client.component.ts
@@ -1,6 +1,26 @@
 import { Component } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+
+import { ClientViewModel       } from '../client';
+import { UpdateClientViewModel } from './update-client.view-model';
+
 @Component({
   templateUrl: './update-client.component.html',
+  providers: [UpdateClientViewModel],
 })
-export class UpdateClientComponent { }
+export class UpdateClientComponent {
+  private readonly subscription: Subscription;
+
+  public constructor(private readonly vm: UpdateClientViewModel) {
+    this.subscription = new Subscription();
+  }
+
+  public get client(): ClientViewModel {
+    return this.vm.client;
+  }
+
+  public ok(): void {
+    this.subscription.add(this.vm.update().subscribe());
+  }
+}
