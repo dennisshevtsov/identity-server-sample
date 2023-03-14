2a7d73feabb983b0999045d6a81de3abb715913f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-15 00:04:02 +0300

    add update clinet method to client service

--- src/identity-server-sample-web-app/src/app/client/services/client.service.ts
+++ src/identity-server-sample-web-app/src/app/client/services/client.service.ts
@@ -7,10 +7,11 @@ import { from       } from 'rxjs';
 import { Observable } from 'rxjs';
 import { switchMap  } from 'rxjs';
 
-import { AddClientRequestDto   } from '../dtos';
-import { GetClientRequestDto   } from '../dtos';
-import { GetClientResponseDto  } from '../dtos';
-import { GetClientsResponseDto } from '../dtos';
+import { AddClientRequestDto    } from '../dtos';
+import { GetClientRequestDto    } from '../dtos';
+import { GetClientResponseDto   } from '../dtos';
+import { GetClientsResponseDto  } from '../dtos';
+import { UpdateClientRequestDto } from '../dtos';
 
 @Injectable({
   providedIn: 'root'
@@ -61,6 +62,21 @@ export class ClientService {
       };
 
       return this.http.post<void>(url, body, options);
+    }));
+  }
+
+  public updateClient(requestDto: UpdateClientRequestDto): Observable<void> {
+    return from(this.um.getUser()).pipe(switchMap(user => {
+      const url = `api/client/${requestDto.clientName}`;
+      const body = JSON.stringify(requestDto);
+      const options = {
+        headers: {
+          'Content-Type' : 'application/json',
+          'Authorization': `Bearer ${user?.access_token}`,
+        },
+      };
+
+      return this.http.put<void>(url, body, options);
     }));
   }
 }
