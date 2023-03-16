0da5dbc52400d36cbee90700ba74d14f11e90f38
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-16 11:11:01 +0300

    add update scope view mode to update scope component

--- src/identity-server-sample-web-app/src/app/scope/components/update-scope/update-scope.component.ts
+++ src/identity-server-sample-web-app/src/app/scope/components/update-scope/update-scope.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { OnDestroy } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { ScopeViewModel       } from '../scope';
+import { UpdateScopeViewModel } from './update-scope.view-model';
 
 @Component({
   templateUrl: './update-scope.component.html',
+  providers: [UpdateScopeViewModel],
 })
-export class UpdateScopeComponent {
+export class UpdateScopeComponent implements OnDestroy {
+  private readonly sub: Subscription;
 
+  public constructor(private readonly vm: UpdateScopeViewModel) {
+    this.sub = new Subscription();
+  }
+
+  public ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  public get scope(): ScopeViewModel {
+    return this.vm.scope;
+  }
+
+  public ok(): void {
+    this.sub.add(this.vm.update().subscribe());
+  }
 }
