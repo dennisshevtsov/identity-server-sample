19feb8ad7fb6149d426a9083d5821059005c5b51
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-16 11:21:50 +0300

    add initialize method to update scope view model

--- src/identity-server-sample-web-app/src/app/scope/components/update-scope/update-scope.view-model.ts
+++ src/identity-server-sample-web-app/src/app/scope/components/update-scope/update-scope.view-model.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
 
+import { map        } from 'rxjs';
 import { Observable } from 'rxjs';
 import { of         } from 'rxjs';
 
+import { ScopeService   } from '../../services';
 import { ScopeViewModel } from '../scope';
 
 @Injectable()
 export class UpdateScopeViewModel {
   private scopeValue: undefined | ScopeViewModel;
 
+  public constructor(private readonly service: ScopeService) {}
+
   public get scope(): ScopeViewModel {
     return this.scopeValue ?? (this.scopeValue = new ScopeViewModel());
+  }
+
+  public initialize(): Observable<void> {
+    return this.service.getScope(this.scope.scopeName)
+                       .pipe(map(responseDto => {
+                         this.scopeValue = new ScopeViewModel(
+                           responseDto.scopeName,
+                           responseDto.displayName,
+                         );
+                       }));
   }
 
   public update(): Observable<void> {
