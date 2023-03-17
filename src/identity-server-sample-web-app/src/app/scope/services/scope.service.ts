b464603b059ebb8d42c3a14292dab538bcf8d4ad
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-17 17:13:25 +0300

    add update method to scope service

--- src/identity-server-sample-web-app/src/app/scope/services/scope.service.ts
+++ src/identity-server-sample-web-app/src/app/scope/services/scope.service.ts
@@ -3,9 +3,10 @@ import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
 
-import { AddScopeRequestDto   } from '../dtos';
-import { GetScopeResponseDto  } from '../dtos';
-import { GetScopesResponseDto } from '../dtos';
+import { AddScopeRequestDto    } from '../dtos';
+import { GetScopeResponseDto   } from '../dtos';
+import { GetScopesResponseDto  } from '../dtos';
+import { UpdateScopeRequestDto } from '../dtos';
 
 @Injectable({
   providedIn: 'root',
@@ -18,6 +19,13 @@ export class ScopeService {
     const body = JSON.stringify(requestDto);
 
     return this.http.post<void>(url, body);
+  }
+
+  public updateScope(requestDto: UpdateScopeRequestDto): Observable<void> {
+    const url = `api/scope/${requestDto.displayName}`;
+    const body = JSON.stringify(requestDto);
+
+    return this.http.put<void>(url, body);
   }
 
   public getScopes(): Observable<GetScopesResponseDto> {
