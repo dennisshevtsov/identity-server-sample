ea9698e45f947ed95c2b199af09f7cc153e6e73d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-17 17:56:07 +0300

    add initialize method to update audience view model

--- src/identity-server-sample-web-app/src/app/audience/components/update-audience/update-audience.view-model.ts
+++ src/identity-server-sample-web-app/src/app/audience/components/update-audience/update-audience.view-model.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 import { UpdateAudienceRequestDto } from '../../dtos';
 import { AudienceService          } from '../../services';
@@ -14,6 +14,18 @@ export class UpdateAudienceViewModel {
 
   public get audience(): AudienceViewModel {
     return this.audienceValue ?? (this.audienceValue = new AudienceViewModel());
+  }
+
+  public initialize(): Observable<void> {
+    return this.service.getAudience(this.audience.audienceName)
+                       .pipe(map(responseDto => {
+                         this.audienceValue = new AudienceViewModel(
+                           responseDto.audienceName,
+                           responseDto.displayName,
+                           responseDto.description,
+                           [...responseDto.scopes],
+                         );
+                       }));
   }
 
   public update(): Observable<void> {
