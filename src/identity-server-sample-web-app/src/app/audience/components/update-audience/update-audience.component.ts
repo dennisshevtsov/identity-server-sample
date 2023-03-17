c1f6562b7d6cb1424259cc6c37ddfd8f00bcbea4
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-17 17:28:55 +0300

    add audience component to update audience component

--- src/identity-server-sample-web-app/src/app/audience/components/update-audience/update-audience.component.ts
+++ src/identity-server-sample-web-app/src/app/audience/components/update-audience/update-audience.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { AudienceViewModel } from '../audience/audience.view-model';
 
 @Component({
   templateUrl: './update-audience.component.html',
 })
 export class UpdateAudienceComponent {
+  public get audience(): AudienceViewModel {
+    return new AudienceViewModel();
+  }
 
+  public ok(): void {}
 }
