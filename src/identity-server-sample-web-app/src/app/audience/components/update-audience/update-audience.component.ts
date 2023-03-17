bf6b2816742aeda284439a4e86fe91c70566b555
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-17 17:37:09 +0300

    add upate audience view model to update audience component

--- src/identity-server-sample-web-app/src/app/audience/components/update-audience/update-audience.component.ts
+++ src/identity-server-sample-web-app/src/app/audience/components/update-audience/update-audience.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { AudienceViewModel } from '../audience/audience.view-model';
+import { OnDestroy } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { AudienceViewModel       } from '../audience';
+import { UpdateAudienceViewModel } from './update-audience.view-model';
 
 @Component({
   templateUrl: './update-audience.component.html',
+  providers: [UpdateAudienceViewModel],
 })
-export class UpdateAudienceComponent {
-  public get audience(): AudienceViewModel {
-    return new AudienceViewModel();
+export class UpdateAudienceComponent implements OnDestroy {
+  private sub: Subscription;
+
+  public constructor(private readonly vm: UpdateAudienceViewModel) {
+    this.sub = new Subscription();
   }
 
-  public ok(): void {}
+  public ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  public get audience(): AudienceViewModel {
+    return this.vm.audience;
+  }
+
+  public ok(): void {
+    this.sub.add(this.vm.update().subscribe());
+  }
 }
