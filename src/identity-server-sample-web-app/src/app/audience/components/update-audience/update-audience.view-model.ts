a9451110e7a3a8e6a3a65cea32abfc7075e5dd3c
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-17 17:52:41 +0300

    update audience via service in view model

--- src/identity-server-sample-web-app/src/app/audience/components/update-audience/update-audience.view-model.ts
+++ src/identity-server-sample-web-app/src/app/audience/components/update-audience/update-audience.view-model.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { of         } from 'rxjs';
 
-import { AudienceViewModel } from '../audience';
+import { UpdateAudienceRequestDto } from '../../dtos';
+import { AudienceService          } from '../../services';
+import { AudienceViewModel        } from '../audience';
 
 @Injectable()
 export class UpdateAudienceViewModel {
   private audienceValue: undefined | AudienceViewModel;
+
+  public constructor(private readonly service: AudienceService) {}
 
   public get audience(): AudienceViewModel {
     return this.audienceValue ?? (this.audienceValue = new AudienceViewModel());
   }
 
   public update(): Observable<void> {
-    return of(void 0);
+    const requestDto = new UpdateAudienceRequestDto(
+      this.audience.audienceName,
+      this.audience.displayName,
+      this.audience.description,
+      [...this.audience.scopes],
+    );
+
+    return this.service.updateAudience(requestDto);
   }
 }
